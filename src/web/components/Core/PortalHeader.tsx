f53cf0965bba08c769a142c22e6276ceddb72bb0
Lionell Pack <lionell.pack@thetradedesk.com>
2023-02-17 14:49:19 +1100

    Add general styling across the whole site.
    Make some lint rules more friendly.
    Switch crypto libraries to hopefully avoid intermittent dev problems.

--- src/web/components/Core/PortalHeader.tsx
+++ src/web/components/Core/PortalHeader.tsx
@@ -1,55 +1,79 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Avatar, AvatarImage } from '@radix-ui/react-avatar';
+import {
+  DropdownMenu,
+  DropdownMenuArrow,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@radix-ui/react-dropdown-menu';
 import * as Switch from '@radix-ui/react-switch';
-import { createHash } from 'crypto';
+import MD5 from 'crypto-js/md5';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './PortalHeader.scss';
 
 export type PortalHeaderProps = {
-  username: string | undefined;
-  setDarkMode: (darkMode: boolean) => void;
+  email: string | undefined;
+  fullname: string | undefined;
+  setDarkMode?: (darkMode: boolean) => void;
 };
 
-export function PortalHeader({ username, setDarkMode }: PortalHeaderProps) {
-  const emailMd5 = username ? createHash('md5').update(username).digest('hex') : null;
-  const [darkToggleState, setDarkToggleState] = useState(true);
+export function PortalHeader({ email, fullname, setDarkMode = undefined }: PortalHeaderProps) {
+  const emailMd5 = email ? MD5(email) : null;
+  const [darkToggleState, setDarkToggleState] = useState(false);
   const onThemeToggle = () => {
     setDarkToggleState(!darkToggleState);
   };
   useEffect(() => {
-    setDarkMode(darkToggleState);
+    setDarkMode?.(darkToggleState);
   }, [darkToggleState, setDarkMode]);
 
   return (
     <div className='portal-header' role='banner'>
       <div className='title'>
         <Link data-testid='title-link' to='/'>
-          UID2 Portal
+          <img src='/uid2-logo.png' alt='UID2 logo' className='uid2-logo' />
         </Link>
       </div>
-      <div className='theme-switch'>
-        <label htmlFor='dark-mode'>
-          Dark mode
-          <Switch.Root
-            name='dark-mode'
-            checked={darkToggleState}
-            onCheckedChange={onThemeToggle}
-            className='theme-toggle'
-          >
-            <Switch.Thumb className='thumb' />
-          </Switch.Root>
-        </label>
-      </div>
-      <Avatar className='portal-avatar'>
-        {!!username && (
-          <AvatarImage
-            src={`//www.gravatar.com/avatar/${emailMd5}.jpg`}
-            alt={`Profile image for ${username}`}
-            onLoadingStatusChange={() => 'loaded'}
-          />
-        )}
-      </Avatar>
+      <DropdownMenu defaultOpen={false}>
+        <DropdownMenuTrigger className='profile-dropdown-button'>
+          {fullname}
+          <FontAwesomeIcon icon='chevron-down' />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='profile-dropdown-content' align='end'>
+          <DropdownMenuArrow className='profile-dropdown-arrow' />
+          <div className='portal-avatar-container'>
+            <Avatar className='portal-avatar' asChild>
+              {!!email && (
+                <AvatarImage
+                  src={`//www.gravatar.com/avatar/${emailMd5}.jpg`}
+                  alt={`Profile image for ${email}`}
+                  onLoadingStatusChange={() => 'loaded'}
+                />
+              )}
+            </Avatar>
+          </div>
+          <DropdownMenuSeparator className='separator' />
+          <DropdownMenuItem onSelect={(event) => event.preventDefault()}>
+            <div className='theme-switch'>
+              <label htmlFor='dark-mode'>Dark mode</label>
+              <Switch.Root
+                name='dark-mode'
+                checked={darkToggleState}
+                onCheckedChange={onThemeToggle}
+                className='theme-toggle'
+              >
+                <Switch.Thumb className='thumb' />
+              </Switch.Root>
+            </div>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator className='separator' />
+          <DropdownMenuItem>Log out</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
