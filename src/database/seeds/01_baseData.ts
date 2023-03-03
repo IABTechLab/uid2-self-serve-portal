57eacccf3cf96341b766c8759a99584ae840d0c1
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-03-03 14:09:02 +1100

    Update migrations and seeds

--- src/database/seeds/01_baseData.ts
+++ src/database/seeds/01_baseData.ts
@@ -22,7 +22,7 @@ const sampleData: Optional<ParticipantTypeModel, 'id'>[] = [
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   await knex('participants_types').del();
-
+  await knex.schema.raw(`DBCC CHECKIDENT ('participants_types', RESEED, 0)`);
   // Inserts seed entries
   await knex('participants_types').insert(sampleData);
 }
