04bcb785ef756b2837c0aeadd6a3f4867e8e66d6
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-03-08 19:26:40 +1100

    fix the redirection issue

--- src/web/contexts/CurrentUserProvider.tsx
+++ src/web/contexts/CurrentUserProvider.tsx
@@ -6,9 +6,13 @@ import { GetUserAccountByEmail, UserAccount } from '../services/userAccount';
 
 type UserContextWithSetter = {
   LoggedInUser: UserAccount | null;
+  loadUser: () => Promise<void>;
 };
 export const CurrentUserContext = createContext<UserContextWithSetter>({
   LoggedInUser: null,
+  loadUser: async () => {
+    'Unable to load user';
+  },
 });
 
 function CurrentUserProvider({ children }: { children: ReactNode }) {
@@ -18,31 +22,34 @@ function CurrentUserProvider({ children }: { children: ReactNode }) {
   const navigate = useNavigate();
   const kcToken = keycloak?.token ?? '';
 
+  const loadUser = useCallback(async () => {
+    const profile = await keycloak.loadUserProfile();
+    const user = await GetUserAccountByEmail(profile?.email);
+    SetLoggedInUser({
+      profile,
+      user,
+    });
+  }, [keycloak]);
+
   useEffect(() => {
     if (LoggedInUser && !LoggedInUser.user && location.pathname !== '/accont/create') {
       navigate('/account/create');
     }
   }, [LoggedInUser, location.pathname, navigate]);
 
-  const userContext = useMemo(
-    () => ({
-      LoggedInUser,
-    }),
-    [LoggedInUser]
-  );
   useEffect(() => {
-    const loadUser = async () => {
-      const profile = await keycloak.loadUserProfile();
-      const user = await GetUserAccountByEmail(profile?.email);
-      SetLoggedInUser({
-        profile,
-        user,
-      });
-    };
     if (kcToken) {
       loadUser();
     }
-  }, [kcToken, keycloak, SetLoggedInUser]);
+  }, [kcToken, keycloak, SetLoggedInUser, loadUser]);
+
+  const userContext = useMemo(
+    () => ({
+      LoggedInUser,
+      loadUser,
+    }),
+    [LoggedInUser, loadUser]
+  );
 
   return <CurrentUserContext.Provider value={userContext}>{children}</CurrentUserContext.Provider>;
 }
