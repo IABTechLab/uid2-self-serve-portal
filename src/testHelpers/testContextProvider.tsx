ba00a4a09c5c906f13153db35272ba4d590c4986
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-03-09 10:06:58 +1100

    cleanup

--- src/testHelpers/testContextProvider.tsx
+++ src/testHelpers/testContextProvider.tsx
@@ -1,6 +1,16 @@
+import { ReactKeycloakProvider } from '@react-keycloak/web';
 import { PropsWithChildren } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import { CurrentUserProvider } from '../web/contexts/CurrentUserProvider';
+import keycloak from '../web/Keycloak';
+
 export function TestContextProvider({ children }: PropsWithChildren) {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <ReactKeycloakProvider authClient={keycloak}>
+      <CurrentUserProvider>
+        <BrowserRouter>{children}</BrowserRouter>
+      </CurrentUserProvider>
+    </ReactKeycloakProvider>
+  );
 }
