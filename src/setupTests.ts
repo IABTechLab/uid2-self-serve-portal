a21fd6ccd18203ec7688030b8d3064458864c989
Lionell Pack <lionell.pack@thetradedesk.com>
2023-03-13 17:16:57 +1100

    Make FontAwesome icons show up in Storybook & consolidate the configuration.
    Update testing-library as user-events wasn't working in the older version.
    Add a polyfill for resize observer when running Jest tests.
    Make our portal header tests more meaningful & test the dropdown functionality.
    Make the portal header behave better if email or name isn't available (i.e. there is probably no user logged in).

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,20 +4,14 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faChevronDown,
-  faEllipsisH,
-  faPencil,
-  faTrashCan,
-} from '@fortawesome/free-solid-svg-icons';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { setGlobalConfig } from '@storybook/testing-react';
 
 import * as globalStorybookConfig from '../.storybook/preview';
+import { configureFontAwesomeLibrary } from './web/configureFontAwesomeLibrary';
 
 setGlobalConfig(globalStorybookConfig);
-library.add(faEllipsisH);
-library.add(faPencil);
-library.add(faTrashCan);
-library.add(faChevronDown);
+configureFontAwesomeLibrary();
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, import/no-extraneous-dependencies
+global.ResizeObserver = require('resize-observer-polyfill');
