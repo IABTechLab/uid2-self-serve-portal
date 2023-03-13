20372b6beaf1b0a46b8e32fe185292b5042540ea
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-03-14 10:20:08 +1100

    test

--- src/web/screens/routeUtils.tsx
+++ src/web/screens/routeUtils.tsx
@@ -3,17 +3,28 @@ import { LoaderFunctionArgs, RouteObject } from 'react-router-dom';
 
 import { PrivateRoute } from './PrivateRoute';
 
+function IsPortalRoute(route: PortalRoute | RouteObject): route is PortalRoute {
+  return !!route.element;
+}
+
+function FakeComponent() {
+  return <div />;
+}
+
 export type PortalRoute = RouteObject & {
   path: string;
   element: JSX.Element;
   description: string;
 };
 
-export const makePrivateRoute = (route: PortalRoute): PortalRoute => {
+export const makePrivateRoute = (route: PortalRoute | RouteObject): PortalRoute => {
+  const x = route.children?.map(makePrivateRoute);
+  if (!IsPortalRoute(route)) throw Error(`Can only make PortalRoutes private!`);
+  if (route.index) return route;
   return {
     ...route,
     element: <PrivateRoute>{route.element}</PrivateRoute>,
-    children: route.children?.map(makePrivateRoute)
+    children: route.index ? undefined : route.children?.map(makePrivateRoute),
     loader: route.loader
       ? async (args: LoaderFunctionArgs) => {
           // await keycloak
