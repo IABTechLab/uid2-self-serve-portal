ac1f588223ec71985b2ef916967b4b4ddbf49a0c
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-03-16 15:13:26 +1100

    move the comment to the right place

--- src/web/components/Core/Notification.spec.tsx
+++ src/web/components/Core/Notification.spec.tsx
@@ -6,11 +6,11 @@ import * as stories from './Notification.stories';
 const { WithIcon, WithTitle, WithoutIconAndTitle } = composeStories(stories);
 
 test('has icon rendered', async () => {
+  // No actual way to tell because we don't load the image in Jest - this is to provide more examples of Jest using Storybook data
   render(<WithIcon />);
 });
 
 test('has title rendered', async () => {
-  // No actual way to tell because we don't load the image in Jest - this is to provide more examples of Jest using Storybook data
   render(<WithTitle />);
   const title = await screen.findByTestId('notification-title');
   expect(title.textContent).toContain('An title');
