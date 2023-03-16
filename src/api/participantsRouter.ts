e587eb4998fe70e71b8373a88d9f486edf7fa89c
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-03-16 14:34:32 +1100

    Add server error handling

--- src/api/participantsRouter.ts
+++ src/api/participantsRouter.ts
@@ -11,10 +11,17 @@ participantsRouter.get('/', async (_req, res) => {
 });
 
 participantsRouter.post('/', async (req, res) => {
-  const data = ParticipantSchema.parse(req.body);
-  // insertGraphAndFetch will implicitly create a transaction
-  const newParticipant = await Participant.query().insertGraphAndFetch([data], { relate: true });
-  res.status(201).json(newParticipant);
+  try {
+    const data = ParticipantSchema.parse(req.body);
+    // insertGraphAndFetch will implicitly create a transaction
+    const newParticipant = await Participant.query().insertGraphAndFetch([data], { relate: true });
+    return res.status(201).json(newParticipant);
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      return res.status(400).send(err.issues);
+    }
+    return res.status(400).send('Unable to create participant');
+  }
 });
 
 const idParser = z.object({
