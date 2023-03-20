ac6cb483301363652e2d0320174f7e7277ad2b68
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-03-21 08:26:29 +1100

    revert changes

--- keycloak/scripts/run_import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Importing self-serve-portal..."
-/opt/keycloak/keycloak/bin/kc.sh import --dir=/opt/keycloak/data/import/ --override true 2>/dev/null 
-
-### Resume normal execution
-/opt/keycloak/scripts/keycloak/run.sh
