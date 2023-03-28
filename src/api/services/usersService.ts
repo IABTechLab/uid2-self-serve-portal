a655d2db01fcc99dcb3ed34947685e9c434b6b9e
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-03-28 16:11:29 +1100

    add error handling

--- src/api/services/usersService.ts
+++ src/api/services/usersService.ts
@@ -1,31 +1,16 @@
-import { RequiredActionAlias } from '@keycloak/keycloak-admin-client/lib/defs/requiredActionProviderRepresentation';
-
 import { User, UserRole } from '../entities/User';
-import { SSP_KK_SSL_RESOURCE, SSP_WEB_BASE_URL } from '../envars';
-import { getKcAdminClient } from '../keycloakAdminClient';
 
-export const inviteNewUser = async (
-  firstName: string,
-  lastName: string,
+export const findUserByEmail = async (email: string) => {
+  return User.query().findOne('email', email);
+};
+export const createUserInPortal = async (
   email: string,
   jobFunction: UserRole,
   participantId: string
 ) => {
-  const kcAdminClient = await getKcAdminClient();
-  const user = await kcAdminClient.users.create({
-    firstName,
-    lastName,
-    email,
-    emailVerified: false,
-    enabled: true,
-  });
+  const user = findUserByEmail(email);
+  if (user) return user;
 
-  await kcAdminClient.users.executeActionsEmail({
-    id: user.id,
-    clientId: SSP_KK_SSL_RESOURCE,
-    actions: [RequiredActionAlias.UPDATE_PASSWORD],
-    redirectUri: SSP_WEB_BASE_URL,
-  });
   const userObject = {
     email,
     role: jobFunction,
