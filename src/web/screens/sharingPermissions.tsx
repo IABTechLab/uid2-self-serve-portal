c0e66a9514f6283a6d80c30b54cb313709958f12
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-05-23 01:05:38 +1000

    Build search and add participant dialog

---
+++ src/web/screens/sharingPermissions.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+import { StatusPopup } from '../components/Core/StatusPopup';
+import { SharingPermissionsTable } from '../components/SharingPermission/SharingPermissionsTable';
+import { PortalRoute } from './routeUtils';
+import { SearchAndAddParticipants } from './searchAndAddParticipantsDialog';
+
+function SharingPermissions() {
+  const [showStatusPopup, setShowStatusPopup] = useState(false);
+  const handleSharingPermissionsAdded = () => {
+    setShowStatusPopup(true);
+  };
+
+  return (
+    <div>
+      <h1>Sharing Permissions</h1>
+      <p>
+        Adding a sharing permission only allows the participant you&apos;re sharing with to decrypt
+        your encrypted UID2s.
+        <br />
+        <br />
+        <b>Please note - this only allows the sharing permission to be enabled, no data is sent.</b>
+      </p>
+      <SearchAndAddParticipants onSharingPermissionsAdded={handleSharingPermissionsAdded} />
+      <SharingPermissionsTable sharedParticipants={[]} />
+      {showStatusPopup && (
+        <StatusPopup status='Success' message='1 Participant added to Your Sharing Permissions' />
+      )}
+    </div>
+  );
+}
+
+export const SharingPermissionsRoute: PortalRoute = {
+  description: 'Sharing Permissions',
+  element: <SharingPermissions />,
+  path: '/dashboard/sharing',
+};
