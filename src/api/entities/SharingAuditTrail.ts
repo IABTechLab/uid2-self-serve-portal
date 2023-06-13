732aace5a42d6d36fc401046f289b4e2a09a6d24
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-06-13 13:35:35 +1000

    Update schema and add audit trail

---
+++ src/api/entities/SharingAuditTrail.ts
@@ -0,0 +1,36 @@
+import { Model } from 'objection';
+
+import { BaseModel } from './BaseModel';
+
+export enum SharingAction {
+  Add = 'add',
+  Delete = 'delete',
+}
+
+export class SharingAuditTrail extends BaseModel {
+  static get tableName() {
+    return 'sharingAuditTrails';
+  }
+  static relationMappings = {
+    participant: {
+      relation: Model.BelongsToOneRelation,
+      modelClass: 'Participant',
+      join: {
+        from: 'sharingAuditTrails.participantId',
+        to: 'participants.id',
+      },
+    },
+    user: {
+      relation: Model.BelongsToOneRelation,
+      modelClass: 'User',
+      join: {
+        from: 'sharingAuditTrails.userId',
+        to: 'users.id',
+      },
+    },
+  };
+
+  id!: number;
+  sharingParticipantId!: number;
+  action!: SharingAction;
+}
---
+++ src/database/migrations/20230613000210_AddSiteIdToParticipantsTableAndCreateSharingAuditTrailTable.ts
@@ -0,0 +1,24 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('participants', (table) => {
+    table.integer('siteId').unique();
+  });
+
+  await knex.schema.createTable('sharingAuditTrails', (table) => {
+    table.increments('id').primary();
+    table.integer('participantId').references('participants.id').onDelete('CASCADE');
+    table.integer('userId').references('users.id').onDelete('CASCADE');
+    table.integer('sharingParticipantId').references('participants.id');
+    table.enu('action', ['add', 'delete']);
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('sharingAuditTrails');
+  await knex.schema.alterTable('participants', (table) => {
+    table.dropUnique([], 'participants_siteid_unique');
+    table.dropColumn('siteId');
+  });
+}
