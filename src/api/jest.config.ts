5303177ccfc48bfa3ffea85812ca380ed679163a
Lionell Pack <lionell.pack@thetradedesk.com>
2023-06-20 17:14:34 +1000

    Add missing ** in path glob.

--- src/api/jest.config.ts
+++ src/api/jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   rootDir: '../../',
   setupFiles: ['dotenv/config', './src/api/jest.setup.ts'],
-  testMatch: ['**/api/*.test.ts', '**/api/*.spec.ts'],
+  testMatch: ['**/api/**/*.test.ts', '**/api/**/*.spec.ts'],
 };
 
 export default config;
