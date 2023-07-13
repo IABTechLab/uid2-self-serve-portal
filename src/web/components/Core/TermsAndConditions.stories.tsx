34ca58c278f426f9cd04c1dcb5ac118917348e1d
Lionell Pack <lionell.pack@thetradedesk.com>
2023-07-13 18:16:56 +1000

    Update stories.

--- src/web/components/Core/TermsAndConditions.stories.tsx
+++ src/web/components/Core/TermsAndConditions.stories.tsx
@@ -1,18 +1,23 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { TermsAndConditionsForm } from './TermsAndConditions';
+import { TermsAndConditions, TermsAndConditionsForm } from './TermsAndConditions';
 
 export default {
-  title: 'Shared Components/Terms and Conditions Dialog',
+  title: 'Shared Components/Terms and Conditions',
   component: TermsAndConditionsForm,
-} as ComponentMeta<typeof TermsAndConditionsForm>;
+} as ComponentMeta<typeof TermsAndConditionsForm | typeof TermsAndConditions>;
 
 const Template: ComponentStory<typeof TermsAndConditionsForm> = (args) => (
   <TermsAndConditionsForm {...args} />
 );
 
-export const Default = Template.bind({});
-Default.args = {
+export const Dialog = Template.bind({});
+Dialog.args = {
   onAccept: () => {},
   onCancel: () => {},
 };
+
+const PageTemplate: ComponentStory<typeof TermsAndConditions> = (args) => (
+  <TermsAndConditions {...args} />
+);
+export const Page = PageTemplate.bind({});
