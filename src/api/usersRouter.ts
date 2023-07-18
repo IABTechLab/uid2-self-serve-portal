7020f6a761812900675f272465415e56c25b611a
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-07-18 16:40:19 +1000

    Add endpoint to update or delete user

--- src/api/usersRouter.ts
+++ src/api/usersRouter.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { z } from 'zod';
 
-import { User, UserCreationPartial } from './entities/User';
+import { User, UserCreationPartial, UserRole } from './entities/User';
 import { getLoggers } from './helpers/loggingHelpers';
 import { getKcAdminClient } from './keycloakAdminClient';
 import { queryUsersByEmail, sendInviteEmail } from './services/kcUsersService';
@@ -35,23 +35,23 @@ export function createUsersRouter() {
     res.status(201).json(user);
   });
 
-usersRouter.put('/current/acceptTerms', enrichCurrentUser, async (req: UserRequest, res) => {
-  await req.user!.$query().patch({ acceptedTerms: true });
-  return res.sendStatus(200);
+  usersRouter.put('/current/acceptTerms', enrichCurrentUser, async (req: UserRequest, res) => {
+    await req.user!.$query().patch({ acceptedTerms: true });
+    return res.sendStatus(200);
   });
 
-usersRouter.use('/:userId', enrichWithUserFromParams);
+  usersRouter.use('/:userId', enrichWithUserFromParams);
 
-usersRouter.get('/:userId', async (req: UserRequest, res) => {
-  return res.status(200).json(req.user);
+  usersRouter.get('/:userId', async (req: UserRequest, res) => {
+    return res.status(200).json(req.user);
   });
 
-usersRouter.get('/:userId/participant', async (req: UserRequest, res) => {
-const participant = await req.user!.$relatedQuery('participant').withGraphFetched('types');
-  return res.status(200).json(participant);
+  usersRouter.get('/:userId/participant', async (req: UserRequest, res) => {
+    const participant = await req.user!.$relatedQuery('participant').withGraphFetched('types');
+    return res.status(200).json(participant);
   });
 
-usersRouter.post('/:userId/resendInvitation', async (req: UserRequest, res) => {
+  usersRouter.post('/:userId/resendInvitation', async (req: UserRequest, res) => {
     const [logger, errorLogger] = getLoggers();
 
     const kcAdminClient = await getKcAdminClient();
@@ -62,15 +62,33 @@ usersRouter.post('/:userId/resendInvitation', async (req: UserRequest, res) => {
       return res.sendStatus(404);
     }
     if (resultLength > 1) {
-        errorLogger.error(
-          `Multiple results received when loading user entry for ${req.user?.email}`
-        );
+      errorLogger.error(`Multiple results received when loading user entry for ${req.user?.email}`);
       return res.sendStatus(500);
     }
 
     logger.info(`Resending invitation email for ${req.user?.email}, keycloak ID ${user[0].id}`);
     await sendInviteEmail(kcAdminClient, user[0]);
     return res.sendStatus(200);
-});
+  });
+
+  usersRouter.delete('/:userId', async (req: UserRequest, res) => {
+    const { user } = req;
+    await user!.$query().delete();
+    return res.sendStatus(200);
+  });
+
+  const UpdateUserParser = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    role: z.nativeEnum(UserRole),
+  });
+
+  usersRouter.put('/:userId', async (req: UserRequest, res) => {
+    const { user } = req;
+    const data = UpdateUserParser.parse(req.body);
+    await user!.$query().patch(data);
+    return res.sendStatus(200);
+  });
+
   return usersRouter;
 }
