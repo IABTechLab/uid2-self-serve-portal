e773b6698e581226772cdd6d5e689bb78d130f20
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-07-27 14:45:10 +1000

    Filename changes

---
+++ src/web/components/SharingPermission/SearchAndAddParticipants.tsx
@@ -0,0 +1,106 @@
+import { useContext, useMemo, useState } from 'react';
+
+import { ParticipantTypeDTO } from '../../../api/entities/ParticipantType';
+import { AvailableParticipantDTO } from '../../../api/participantsRouter';
+import { ParticipantContext } from '../../contexts/ParticipantProvider';
+import { Dialog } from '../Core/Dialog';
+import { ParticipantSearchBar } from './ParticipantSearchBar';
+
+import './SearchAndAddParticipants.scss';
+
+type SearchAndAddParticipantsProps = {
+  onSharingPermissionsAdded: (selectedSiteIds: number[]) => Promise<void>;
+  sharingParticipants: AvailableParticipantDTO[];
+  availableParticipants: AvailableParticipantDTO[];
+  participantTypes: ParticipantTypeDTO[];
+};
+export function SearchAndAddParticipants({
+  onSharingPermissionsAdded,
+  sharingParticipants,
+  availableParticipants,
+  participantTypes,
+}: SearchAndAddParticipantsProps) {
+  const [openConfirmation, setOpenConfirmation] = useState(false);
+  const [selectedParticipants, setSelectedParticipants] = useState<Set<number>>(new Set());
+  const [openSearchResult, setOpenSearchResult] = useState<boolean>(false);
+  const { participant } = useContext(ParticipantContext);
+  const onHandleAddParticipants = () => {
+    setOpenConfirmation(false);
+    setSelectedParticipants(new Set());
+    onSharingPermissionsAdded(Array.from(selectedParticipants));
+  };
+
+  const sharingParticipantsSiteIds = useMemo(() => {
+    return new Set(sharingParticipants.map((p) => p.siteId));
+  }, [sharingParticipants]);
+
+  const getSearchableParticipants = (resolvedParticipants: AvailableParticipantDTO[]) => {
+    return resolvedParticipants.filter(
+      (p) => p.id !== participant?.id && !sharingParticipantsSiteIds.has(p.siteId)
+    );
+  };
+
+  const getParticipantText = (participantCount: number): string => {
+    if (participantCount === 1) {
+      return '1 Participant';
+    }
+    return `${participantCount} Participants`;
+  };
+
+  const handleSelectedParticipantChanged = (selectedItems: Set<number>) => {
+    setSelectedParticipants(selectedItems);
+    setOpenSearchResult(false);
+  };
+
+  return (
+    <div className='search-and-add-participants'>
+      <div className='add-participant-dialog-search-bar'>
+        <ParticipantSearchBar
+          selectedParticipantIds={selectedParticipants}
+          participants={getSearchableParticipants(availableParticipants)}
+          onSelectedChange={handleSelectedParticipantChanged}
+          participantTypes={participantTypes}
+          open={openSearchResult}
+          onToggle={setOpenSearchResult}
+        />
+      </div>
+      {!openSearchResult && (
+        <div className='action-section'>
+          {selectedParticipants.size > 0 && (
+            <p>{getParticipantText(selectedParticipants.size)} added</p>
+          )}
+          <Dialog
+            title='Please review the following changes'
+            triggerButton={
+              <button
+                type='button'
+                className='primary-button add-participant-button'
+                disabled={!selectedParticipants.size}
+              >
+                Add Participants
+              </button>
+            }
+            open={openConfirmation}
+            onOpenChange={setOpenConfirmation}
+          >
+            <ul className='dot-list'>
+              <li>Adding {getParticipantText(selectedParticipants.size)}</li>
+            </ul>
+            <div className='dialog-footer-section'>
+              <button type='button' className='primary-button' onClick={onHandleAddParticipants}>
+                I acknowledge these changes
+              </button>
+              <button
+                type='button'
+                className='transparent-button'
+                onClick={() => setOpenConfirmation(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </Dialog>
+        </div>
+      )}
+    </div>
+  );
+}
