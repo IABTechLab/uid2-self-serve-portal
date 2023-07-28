c4634bde096945f330a4730e890dd5da152339fc
Alex Yau <alex.yau@thetradedesk.com>
2023-07-28 17:36:24 +1000

    Banner component without icons working

--- src/web/components/Core/Banner.spec.tsx
+++ src/web/components/Core/Banner.spec.tsx
@@ -3,10 +3,10 @@ import { render, screen } from '@testing-library/react';
 
 import * as stories from './Banner.stories';
 
-const { WithInfoMessage } = composeStories(stories);
+const { Info } = composeStories(stories);
 
 test('has icon and message rendered', async () => {
-  render(<WithInfoMessage />);
+  render(<Info />);
   const title = await screen.findByTestId('banner-message');
   expect(title.textContent).toContain('here is a banner');
   expect(screen.getByTestId('banner-icon')).toBeInTheDocument();
--- src/web/components/Core/Banner.tsx
+++ src/web/components/Core/Banner.tsx
@@ -1,3 +1,4 @@
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import clsx from 'clsx';
 
@@ -5,13 +6,23 @@ import './Banner.scss';
 
 type BannerProps = {
   message: string;
-  className?: string;
+  type: 'info' | 'warning' | 'error';
 };
 
-export function Banner({ message, className }: BannerProps) {
+export function Banner({ message, type }: BannerProps) {
+  let icon: IconProp;
+
+  if (type === 'warning') {
+    icon = 'triangle-exclamation';
+  } else if (type === 'error') {
+    icon = 'fishing-rod';
+  } else {
+    icon = 'circle-info';
+  }
+
   return (
-    <div className={clsx('banner-container', className)}>
-      <FontAwesomeIcon icon='circle-info' data-testid='banner-icon' className='banner-icon' />
+    <div className={clsx('banner-container', type)}>
+      <FontAwesomeIcon icon={icon} data-testid='banner-icon' className='banner-icon' />
       <span data-testid='banner-message' className='banner-text'>
         {message}
       </span>
