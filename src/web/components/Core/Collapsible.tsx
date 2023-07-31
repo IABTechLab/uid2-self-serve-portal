0acd6791330dbdadac6609f127da3f72c9f83836
Alex Yau <alex.yau@thetradedesk.com>
2023-07-31 14:06:13 +1000

    Add optional label to Collapsible

--- src/web/components/Core/Collapsible.tsx
+++ src/web/components/Core/Collapsible.tsx
@@ -8,19 +8,23 @@ export type CollapsibleProps = {
   title: string;
   content: ReactNode;
   defaultOpen?: boolean;
+  label?: string;
 };
 
-export function Collapsible({ title, content, defaultOpen }: CollapsibleProps) {
+export function Collapsible({ title, content, defaultOpen, label }: CollapsibleProps) {
   return (
     <RadixCollapsible.Root className='collapsible-root' defaultOpen={defaultOpen}>
       <RadixCollapsible.Trigger className='collapsible-trigger'>
         <div className='collapsible-header'>
           <h2>{title}</h2>
-          <FontAwesomeIcon
-            icon='chevron-down'
-            data-testid='chevron-icon'
-            className='chevron-icon'
-          />
+          <div className='collapsible-header-label-and-icon'>
+            {label && <span className='label'>{label}</span>}
+            <FontAwesomeIcon
+              icon='chevron-down'
+              data-testid='chevron-icon'
+              className='chevron-icon'
+            />
+          </div>
         </div>
       </RadixCollapsible.Trigger>
       <RadixCollapsible.Content>
