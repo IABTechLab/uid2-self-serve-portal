3f6796e3d87a94cb3c1a95c846ac7ebd3b33e652
Alex Yau <alex.yau@thetradedesk.com>
2023-07-31 11:10:19 +1000

    Change error styling, fix test

--- src/web/components/Core/Banner.spec.tsx
+++ src/web/components/Core/Banner.spec.tsx
@@ -8,6 +8,6 @@ const { Info } = composeStories(stories);
 test('has icon and message rendered', async () => {
   render(<Info />);
   const title = await screen.findByTestId('banner-message');
-  expect(title.textContent).toContain('here is a banner');
+  expect(title.textContent).toContain(Info.args?.message);
   expect(screen.getByTestId('banner-icon')).toBeInTheDocument();
 });
