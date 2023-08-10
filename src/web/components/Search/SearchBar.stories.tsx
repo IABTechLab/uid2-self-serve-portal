182445c2c8f107cc5264fecea63b3e4148c5c6f2
Lionell Pack <lionell.pack@thetradedesk.com>
2023-08-10 15:45:51 +1000

    Move some styling to the shared search bar component.
    Add Storybook examples.

---
+++ src/web/components/Search/SearchBar.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { SearchBarContainer, SearchBarInput, SearchBarResults } from './SearchBar';
+
+export default {
+  title: 'Shared Components/SearchBar',
+  component: SearchBarContainer,
+} as ComponentMeta<typeof SearchBarContainer>;
+
+export const basicSearchBar: ComponentStory<typeof SearchBarContainer> = (args) => (
+  <SearchBarContainer {...args}>
+    <SearchBarInput />
+  </SearchBarContainer>
+);
+basicSearchBar.args = {};
+
+export const searchBarWithResults: ComponentStory<typeof SearchBarContainer> = (args) => (
+  <SearchBarContainer {...args}>
+    <SearchBarInput fullBorder />
+    <SearchBarResults>
+      <div style={{ padding: '20px' }}>Results would go here!</div>
+    </SearchBarResults>
+  </SearchBarContainer>
+);
+basicSearchBar.args = {};
