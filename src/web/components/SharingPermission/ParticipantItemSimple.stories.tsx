0387db510c59ffa68960edc679cad21593da44d4
Alex Yau <alex.yau@thetradedesk.com>
2023-08-23 14:41:27 +1000

    ParticipantItemSimple component

---
+++ src/web/components/SharingPermission/ParticipantItemSimple.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ParticipantItemSimple } from './ParticipantItem';
+
+export default {
+  title: 'Sharing Permissions/ParticipantItemSimple',
+  component: ParticipantItemSimple,
+} as ComponentMeta<typeof ParticipantItemSimple>;
+
+const Template: ComponentStory<typeof ParticipantItemSimple> = (args) => (
+  <table>
+    <tbody>
+      <tr>
+        <ParticipantItemSimple {...args} />
+      </tr>
+    </tbody>
+  </table>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  participant: {
+    id: 1,
+    name: 'Participant 1',
+    types: [{ id: 2, typeName: 'Type 2' }],
+  },
+};
