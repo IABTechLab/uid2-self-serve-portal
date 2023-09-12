24fd71a04c50c3447658255182a518369850c065
Scott Sundahl <scott.sundahl@thetradedesk.com>
2023-09-12 14:26:19 -0600

    dereference users rather than delete

---
+++ src/database/migrations/20230912185913_AddDeletedFlagToUsersTable.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.boolean('deleted').defaultTo(false).notNullable();
+  });
+
+  // Update existing rows to have the default value for 'completedRecommendations'
+  await knex('users').update({ deleted: false });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (table) => {
+    table.dropColumn('deleted');
+  });
+}
