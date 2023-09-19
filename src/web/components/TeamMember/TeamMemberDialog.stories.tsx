81986f8c89413a9d4c786e3c87a660576139bf82
Jingyi Gao <jingyi.gao@thetradedesk.com>
2023-09-19 16:35:21 +1000

    Upgrade storybook to V7

--- src/web/components/TeamMember/TeamMemberDialog.stories.tsx
+++ src/web/components/TeamMember/TeamMemberDialog.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 
 import { UserRole } from '../../../api/entities/User';
 import TeamMemberDialog from './TeamMemberDialog';
@@ -6,26 +6,29 @@ import TeamMemberDialog from './TeamMemberDialog';
 export default {
   title: 'Team Member/Team Member Dialog',
   component: TeamMemberDialog,
-} as ComponentMeta<typeof TeamMemberDialog>;
+} as Meta<typeof TeamMemberDialog>;
 
-const Template: ComponentStory<typeof TeamMemberDialog> = (args) => <TeamMemberDialog {...args} />;
+// const Template: StoryObj<typeof TeamMemberDialog> = (args) => <TeamMemberDialog {...args} />;
+type Story = StoryObj<typeof TeamMemberDialog>;
 
-export const Default = Template.bind({});
-Default.args = {
-  triggerButton: <button type='button'>Open</button>,
-  onAddTeamMember: (form) => Promise.resolve(console.log(`Add new user ${JSON.stringify(form)}`)),
+export const Default: Story = {
+  args: {
+    triggerButton: <button type='button'>Open</button>,
+    onAddTeamMember: (form) => Promise.resolve(console.log(`Add new user ${JSON.stringify(form)}`)),
+  },
 };
 
-export const WithTeamMember = Template.bind({});
-WithTeamMember.args = {
-  ...Default.args,
-  person: {
-    id: 1,
-    email: 'test@user.com',
-    firstName: 'test',
-    lastName: 'test',
-    role: UserRole.DA,
-    acceptedTerms: true,
-    participantId: 1,
+export const WithTeamMember: Story = {
+  args: {
+    ...Default.args,
+    person: {
+      id: 1,
+      email: 'test@user.com',
+      firstName: 'test',
+      lastName: 'test',
+      role: UserRole.DA,
+      acceptedTerms: true,
+      participantId: 1,
+    },
   },
 };
