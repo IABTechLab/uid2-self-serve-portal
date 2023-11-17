6768882c2a4cf925582d027c203bf161df243206
Alex Yau <alex.yau@thetradedesk.com>
2023-11-17 14:00:17 +1100

    Rename variables in test

--- src/web/components/SharingPermission/bulkAddPermissionsHelpers.spec.ts
+++ src/web/components/SharingPermission/bulkAddPermissionsHelpers.spec.ts
@@ -3,15 +3,15 @@ import { publisherHasUncheckedDSP } from './bulkAddPermissionsHelpers';
 
 describe('Bulk add permission helper tests', () => {
   describe('#publisherHasUncheckedDSP', () => {
-    const shareWithDsp: ClientType[] = ['DSP'];
-    const publisherType = { id: 2, typeName: 'Publisher' };
+    const dspSharedType: ClientType[] = ['DSP'];
+    const publisherParticipantType = { id: 2, typeName: 'Publisher' };
 
     test.each([
-      [true, publisherType, false, shareWithDsp, false],
-      [true, publisherType, false, shareWithDsp, true],
-      [false, publisherType, false, [], true],
-      [false, { id: 3, typeName: 'Advertiser' }, false, shareWithDsp, false],
-      [false, publisherType, true, shareWithDsp, false],
+      [true, publisherParticipantType, false, dspSharedType, false],
+      [true, publisherParticipantType, false, dspSharedType, true],
+      [false, publisherParticipantType, false, [], true],
+      [false, { id: 3, typeName: 'Advertiser' }, false, dspSharedType, false],
+      [false, publisherParticipantType, true, dspSharedType, false],
     ])(
       'returns %p when participantTypes is %p, DSPChecked is %p, sharedTypes is %p, and completedRecommendations is %p',
       (expected, participantTypes, dspChecked, sharedTypes, completedRecommendations) => {
