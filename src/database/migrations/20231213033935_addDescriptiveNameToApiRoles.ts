b0ee09dc88b9d2996d2508da12e066ec2ad5c2fa
joshua.smee <joshua.smee@thetradedesk.com>
2023-12-13 16:02:07 +1100

    Created migration

---
+++ src/database/migrations/20231213033935_addDescriptiveNameToApiRoles.ts
@@ -0,0 +1,27 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('apiRoles', (table) => {
+    table.string('externalName', 128);
+  });
+
+  await knex('apiRoles').where({ roleName: 'GENERATOR' }).update({ externalName: 'Token Creator' });
+  await knex('apiRoles').where({ roleName: 'MAPPER' }).update({ externalName: 'UID2 Creator' });
+  await knex('apiRoles').where({ roleName: 'ID_READER' }).update({ externalName: 'Decryptor' });
+  await knex('apiRoles').where({ roleName: 'SHARER' }).update({ externalName: 'Securer' });
+  await knex('apiRoles').where({ roleName: 'OPTOUT' }).update({ externalName: 'OPTOUT' });
+
+  await knex('apiRoles')
+    .whereNull('externalName')
+    .update({ externalName: knex.ref('roleName') });
+
+  await knex.schema.alterTable('apiRoles', (table) => {
+    table.dropNullable('externalName');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('apiRoles', (table) => {
+    table.dropColumn('externalName');
+  });
+}
