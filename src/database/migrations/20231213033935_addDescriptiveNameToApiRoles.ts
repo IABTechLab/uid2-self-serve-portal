8df551319e49297cb804732d632248827a0577c2
joshua.smee <joshua.smee@thetradedesk.com>
2023-12-15 16:34:24 +1100

    Updated desrciptive terms

--- src/database/migrations/20231213033935_addDescriptiveNameToApiRoles.ts
+++ src/database/migrations/20231213033935_addDescriptiveNameToApiRoles.ts
@@ -5,10 +5,10 @@ export async function up(knex: Knex): Promise<void> {
     table.string('externalName', 128);
   });
 
-  await knex('apiRoles').where({ roleName: 'GENERATOR' }).update({ externalName: 'Token Creator' });
-  await knex('apiRoles').where({ roleName: 'MAPPER' }).update({ externalName: 'UID2 Creator' });
-  await knex('apiRoles').where({ roleName: 'ID_READER' }).update({ externalName: 'Decryptor' });
-  await knex('apiRoles').where({ roleName: 'SHARER' }).update({ externalName: 'Securer' });
+  await knex('apiRoles').where({ roleName: 'GENERATOR' }).update({ externalName: 'Generator' });
+  await knex('apiRoles').where({ roleName: 'MAPPER' }).update({ externalName: 'Mapper' });
+  await knex('apiRoles').where({ roleName: 'ID_READER' }).update({ externalName: 'Bidder' });
+  await knex('apiRoles').where({ roleName: 'SHARER' }).update({ externalName: 'Sharer' });
   await knex('apiRoles').where({ roleName: 'OPTOUT' }).update({ externalName: 'Opt-Out' });
 
   await knex('apiRoles')
