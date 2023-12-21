1f96216fc4658a0bd676a211807d3f10b87b5fe1
joshua.smee <joshua.smee@thetradedesk.com>
2023-12-21 13:25:56 +1100

    Fixed migration

--- src/database/migrations/20231220235718_removeParticipantIdColumnFromAudit.ts
+++ src/database/migrations/20231220235718_removeParticipantIdColumnFromAudit.ts
@@ -1,23 +1,24 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  knex.raw(
+  await knex.raw(
     `UPDATE auditTrails 
     SET eventData = JSON_MODIFY(eventData ,'$.participantId',participantId) 
     WHERE participantId is not NULL`
   );
 
-  knex.schema.alterTable('auditTrails', (table) => {
+  await knex.schema.alterTable('auditTrails', (table) => {
+    table.dropForeign('participantId');
     table.dropColumn('participantId');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  knex.schema.alterTable('auditTrails', (table) => {
+  await knex.schema.alterTable('auditTrails', (table) => {
     table.integer('participantId').references('participants.id');
   });
 
-  knex.raw(`UPDATE auditTrails 
+  await knex.raw(`UPDATE auditTrails 
             SET participantId = JSON_VALUE(eventData , '$.participantId')
             WHERE JSON_VALUE(eventData , '$.participantId') IS NOT NULL;`);
 }
