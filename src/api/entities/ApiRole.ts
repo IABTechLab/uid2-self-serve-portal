aec803ccc71b01961aa7298339996c7a6984ecea
joshua.smee <joshua.smee@thetradedesk.com>
2024-01-04 16:27:01 +1100

    Renamed functions

--- src/api/entities/ApiRole.ts
+++ src/api/entities/ApiRole.ts
@@ -25,12 +25,14 @@ export class ApiRole extends BaseModel {
   declare id: number;
   declare roleName: string;
   declare externalName: string;
+  declare disabled: boolean;
 }
 
-export type ApiRoleDTO = ModelObjectOpt<ApiRole>;
+export type ApiRoleDTO = Omit<ModelObjectOpt<ApiRole>, 'disabled'>;
 
 export const ApiRoleSchema = z.object({
   id: z.number(),
   roleName: z.string(),
   externalName: z.string(),
+  disabled: z.boolean(),
 });
