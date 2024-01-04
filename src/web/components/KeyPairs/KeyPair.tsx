831e66c3a082040b9da6803b7b001874a52dba42
joshua.smee <joshua.smee@thetradedesk.com>
2024-01-04 15:45:26 +1100

    Removed unneed styling

--- src/web/components/KeyPairs/KeyPair.tsx
+++ src/web/components/KeyPairs/KeyPair.tsx
@@ -10,7 +10,7 @@ function KeyPair({ keyPair }: KeyPairProps) {
     <tr>
       <td className='name'>{keyPair.name}</td>
       <td className='subscription-id'>{keyPair.subscriptionId}</td>
-      <td className='public-key'>
+      <td>
         <KeySecretReveal title='public-key' value={keyPair.publicKey} />
       </td>
       <td className='created'>{keyPair.createdString}</td>
