535cd920ea046307577ea39824508d14cb8933e8
joshua.smee <joshua.smee@thetradedesk.com>
2024-01-08 10:33:06 +1100

    Removed unnecary comment

--- src/web/components/ParticipantManagement/UpdateParticipantDialog.stories.tsx
+++ src/web/components/ParticipantManagement/UpdateParticipantDialog.stories.tsx
@@ -50,7 +50,6 @@ export const ParticipantWithExistingRoles: Story = {
       ],
     },
     onUpdateParticipant: (form) => {
-      // eslint-disable-next-line no-console
       console.log(form);
       return Promise.resolve();
     },
