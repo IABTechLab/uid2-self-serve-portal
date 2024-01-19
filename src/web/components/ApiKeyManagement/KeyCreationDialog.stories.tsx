bbe7d4a79a2f3046a099ada4dee2d7e54190bc29
Joshua Smee <joshua.smee@thetradedesk.com>
2024-01-19 13:34:17 +1100

    Added test

--- src/web/components/ApiKeyManagement/KeyCreationDialog.stories.tsx
+++ src/web/components/ApiKeyManagement/KeyCreationDialog.stories.tsx
@@ -29,3 +29,19 @@ export const MultipleRoles: Story = {
     ],
   },
 };
+
+export const OneRole: Story = {
+  args: {
+    triggerButton: <button type='button'>Open</button>,
+    onKeyCreation: (form) => {
+      // eslint-disable-next-line no-console
+      console.log(`Add a new Key ${JSON.stringify(form)}`);
+      return Promise.resolve({
+        plaintextKey: 'Test_Plaintext',
+        secret: 'Test_Secret',
+        name: 'Test_Key',
+      });
+    },
+    availableRoles: [{ id: 1, roleName: 'MAPPER', externalName: 'Mapper' }],
+  },
+};
