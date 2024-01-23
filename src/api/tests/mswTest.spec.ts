597002631921bf24f16dbf05e75c3bb18a452b67
Joshua Smee <joshua.smee@thetradedesk.com>
2024-01-23 13:03:53 +1100

    Created first test

--- src/api/tests/mswTest.spec.ts
+++ src/api/tests/mswTest.spec.ts
@@ -1,33 +1,34 @@
-import { composeStories } from '@storybook/testing-react';
-import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
 
-const server = setupServer(
+const handlers = [
   http.get(
     // The "/pets" string is a path predicate.
     // Only the GET requests whose path matches
     // the "/pets" string will be intercepted.
-    '/pets',
+    'http://localhost/pets',
     // The function below is a "resolver" function.
     // It accepts a bunch of information about the
     // intercepted request, and decides how to handle it.
     ({ request, params, cookies }) => {
       return HttpResponse.json(['Tom', 'Jerry', 'Spike']);
     }
-  )
-);
+  ),
+];
 
-describe('TESTING', () => {
+const server = setupServer(...handlers);
+
+describe('MSW Test', () => {
   beforeAll(() => server.listen());
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
 
   it('should work', async () => {
+    console.log(server.listHandlers());
+
     const response = await axios.get<String[]>(`/pets`);
 
-    expect(response).toBeCalledWith(['Tom', 'Jerry', 'Spike']);
+    expect(response.data).toEqual(['Tom', 'Jerry', 'Spike']);
   });
 });
