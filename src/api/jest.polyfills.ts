cc01a527fa4676636a602639c3acefe8db2c6090
Joshua Smee <joshua.smee@thetradedesk.com>
2024-01-25 07:57:22 +1100

    Fixed jest.polyfills file

--- src/api/jest.polyfills.ts
+++ src/api/jest.polyfills.ts
@@ -9,20 +9,18 @@
  * you don't want to deal with this.
  */
 
-const { TextDecoder, TextEncoder } = require('node:util');
+import { Blob } from 'node:buffer';
+import { TextDecoder, TextEncoder } from 'node:util';
+import { fetch, FormData, Headers, Request, Response } from 'undici';
 
 Object.defineProperties(globalThis, {
   TextDecoder: { value: TextDecoder },
   TextEncoder: { value: TextEncoder },
 });
 
-const { Blob, File } = require('node:buffer');
-const { fetch, Headers, FormData, Request, Response } = require('undici');
-
 Object.defineProperties(globalThis, {
   fetch: { value: fetch, writable: true },
   Blob: { value: Blob },
-  File: { value: File },
   Headers: { value: Headers },
   FormData: { value: FormData },
   Request: { value: Request },
