8a831e5d1ff93d48df08fd40b927515ee05a34d3
Scott Sundahl <scott.sundahl@thetradedesk.com>
2024-03-21 14:46:17 -0600

    layout changes

--- src/web/components/ParticipantManagement/AddParticipantDialog.tsx
+++ src/web/components/ParticipantManagement/AddParticipantDialog.tsx
@@ -142,139 +142,153 @@ function AddParticipantDialog({
       open={open}
       onOpenChange={setOpen}
       className='add-participant-dialog'
-      fullScreen
+      hideActionCloseButtonOnly
     >
       <RootFormErrors fieldErrors={errors} />
       <FormProvider {...formMethods}>
         <form onSubmit={handleSubmit(submit)}>
-          <h4>Participant Information</h4>
-          <span>Provide the following information about the company/participant.</span>
-          <TextInput
-            inputName='participantName'
-            label='Participant Name'
-            className='text-input'
-            rules={{ required: 'Please specify Participant Name.' }}
-          />
-
-          <MultiCheckboxInput
-            inputName='participantTypes'
-            label='Participant Type'
-            options={participantTypes.map((p) => ({
-              optionLabel: p.typeName,
-              value: p.id,
-            }))}
-            rules={{ required: 'Please specify Participant Type(s).' }}
-          />
-          <div className='site-type'>
-            <RadioInput
-              inputName='siteIdType'
-              label='Site ID'
-              options={[
-                { optionLabel: 'Existing Site ID', value: 0 },
-                { optionLabel: 'New Site ID', value: 1 },
-              ]}
-            />
-          </div>
-          {!newSite && (
-            <div>
-              <SearchBarContainer>
-                <SearchBarFormInput
-                  inputName='siteId'
-                  inputClassName='search-input'
-                  fullBorder
-                  value={
-                    !selectedSite ? searchText : `${selectedSite.name} (Site ID ${selectedSite.id})`
-                  }
-                  onChange={onSearchInputChange}
-                  onFocus={onSearchInputFocus}
-                  label='Search Participant Name to find Site ID'
-                  rules={{ required: 'Please specify Site ID.' }}
-                />
-                {!selectedSite && searchText && (
-                  <SearchBarResults className='site-search-results'>
-                    {siteSearchResults?.map((s) => (
-                      <button
-                        key={s.item.id}
-                        type='button'
-                        className='text-button'
-                        onClick={() => onSiteClick(s.item)}
-                      >
-                        <HighlightedResult result={s} /> (Site ID: {s.item.id})
-                      </button>
-                    ))}
-                  </SearchBarResults>
-                )}
-              </SearchBarContainer>
-            </div>
-          )}
-          {newSite && (
+          <div>
+            <h4>Participant Information</h4>
+            <span>Provide the following information about the company/participant.</span>
             <TextInput
-              inputName='siteName'
-              label='New Site Name'
+              inputName='participantName'
+              label='Participant Name'
               className='text-input'
-              rules={{ required: 'Please specify Site Name.' }}
-              disabled
+              rules={{ required: 'Please specify Participant Name.' }}
             />
-          )}
-          <div className='add-participant-dialog-flex'>
+
             <MultiCheckboxInput
-              inputName='apiRoles'
-              label='API Roles'
-              options={sortApiRoles(apiRoles).map((p) => ({
-                optionLabel: p.externalName,
-                optionToolTip: p.roleName,
+              inputName='participantTypes'
+              label='Participant Type'
+              options={participantTypes.map((p) => ({
+                optionLabel: p.typeName,
                 value: p.id,
               }))}
-              rules={{ required: 'Please specify API Role(s).' }}
+              rules={{ required: 'Please specify Participant Type(s).' }}
             />
-          </div>
-          <div>
-            <h4>Participant Contact Information</h4>
-            <span>
-              Enter the information of the participant who is requesting the account. An email will
-              be sent to the contact with instructions to access the account.
-            </span>
-            <div className='add-participant-dialog-flex'>
-              <div>
-                <TextInput
-                  inputName='firstName'
-                  label='Contact First Name'
-                  className='text-input'
-                  rules={{ required: 'Please specify Contact First Name.' }}
-                />
-              </div>
-              <div className='right-column'>
-                <TextInput
-                  inputName='lastName'
-                  label='Contact Last Name'
-                  className='text-input'
-                  rules={{ required: 'Please specify Contact Last Name.' }}
-                />
-              </div>
-            </div>
-            <TextInput
-              inputName='email'
-              label='Contact Email'
-              className='text-input'
-              rules={{ required: 'Please specify Contact Email.' }}
-            />
-            <div className='user-roles'>
-              <SelectInput
-                inputName='role'
-                label='Job Function'
-                rules={{ required: "Please specify the contact's job function." }}
-                options={(Object.keys(UserRole) as Array<keyof typeof UserRole>).map((key) => ({
-                  optionLabel: UserRole[key],
-                  value: UserRole[key],
-                }))}
+            <div className='site-type'>
+              <RadioInput
+                inputName='siteIdType'
+                label='Site ID'
+                options={[
+                  { optionLabel: 'Existing Site ID', value: 0 },
+                  { optionLabel: 'New Site ID', value: 1 },
+                ]}
               />
             </div>
-          </div>
-          <br />
-          <div className='request-button'>
-            <button type='submit' className='primary-button'>
-              Add Participant
-            </button>
+            {!newSite && (
+              <div>
+                <SearchBarContainer>
+                  <SearchBarFormInput
+                    inputName='siteId'
+                    inputClassName='search-input'
+                    fullBorder
+                    value={
+                      !selectedSite
+                        ? searchText
+                        : `${selectedSite.name} (Site ID ${selectedSite.id})`
+                    }
+                    onChange={onSearchInputChange}
+                    onFocus={onSearchInputFocus}
+                    label='Search Participant Name to find Site ID'
+                    rules={{ required: 'Please specify Site ID.' }}
+                  />
+                  {!selectedSite && searchText && (
+                    <SearchBarResults className='site-search-results'>
+                      {siteSearchResults?.map((s) => (
+                        <button
+                          key={s.item.id}
+                          type='button'
+                          className='text-button'
+                          onClick={() => onSiteClick(s.item)}
+                        >
+                          <HighlightedResult result={s} /> (Site ID: {s.item.id})
+                        </button>
+                      ))}
+                    </SearchBarResults>
+                  )}
+                </SearchBarContainer>
+              </div>
+            )}
+            {newSite && (
+              <TextInput
+                inputName='siteName'
+                label='New Site Name'
+                className='text-input'
+                rules={{ required: 'Please specify Site Name.' }}
+                disabled
+              />
+            )}
+            <div className='add-participant-dialog-flex'>
+              <MultiCheckboxInput
+                inputName='apiRoles'
+                label='API Roles'
+                options={sortApiRoles(apiRoles).map((p) => ({
+                  optionLabel: p.externalName,
+                  optionToolTip: p.roleName,
+                  value: p.id,
+                }))}
+                rules={{ required: 'Please specify API Role(s).' }}
+              />
+            </div>
+            <div>
+              <h4>Participant Contact Information</h4>
+              <span>
+                Enter the information of the participant who is requesting the account. An email
+                will be sent to the contact with instructions to access the account.
+              </span>
+              <div className='add-participant-dialog-flex'>
+                <div>
+                  <TextInput
+                    inputName='firstName'
+                    label='Contact First Name'
+                    className='text-input'
+                    rules={{ required: 'Please specify Contact First Name.' }}
+                  />
+                </div>
+                <div className='right-column'>
+                  <TextInput
+                    inputName='lastName'
+                    label='Contact Last Name'
+                    className='text-input'
+                    rules={{ required: 'Please specify Contact Last Name.' }}
+                  />
+                </div>
+              </div>
+              <div className='add-participant-dialog-flex'>
+                <div>
+                  <TextInput
+                    inputName='email'
+                    label='Contact Email'
+                    className='text-input'
+                    rules={{ required: 'Please specify Contact Email.' }}
+                  />
+                </div>
+                <div className='user-roles right-column'>
+                  <SelectInput
+                    inputName='role'
+                    label='Job Function'
+                    options={(Object.keys(UserRole) as Array<keyof typeof UserRole>).map((key) => ({
+                      optionLabel: UserRole[key],
+                      value: UserRole[key],
+                    }))}
+                  />
+                </div>
+              </div>
+            </div>
+            <br />
+            <div className='action-container'>
+              <div className='request-button'>
+                <button type='submit' className='primary-button'>
+                  Add Participant
+                </button>
+              </div>
+              <div className='cancel-button'>
+                <button type='button' className='transparent-button' onClick={() => setOpen(false)}>
+                  Cancel
+                </button>
+              </div>
+            </div>
           </div>
         </form>
       </FormProvider>
