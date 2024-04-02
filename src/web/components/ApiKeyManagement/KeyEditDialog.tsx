af87039a649940ba3adf9da96996968124e94845
Ashley Smith <ashley.smith@thetradedesk.com>
2024-04-02 12:32:39 -0600

    KeyEditDialog replaced Form with FormProvider

--- src/web/components/ApiKeyManagement/KeyEditDialog.tsx
+++ src/web/components/ApiKeyManagement/KeyEditDialog.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react';
-import { SubmitHandler } from 'react-hook-form';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 
 import { ApiRoleDTO } from '../../../api/entities/ApiRole';
 import { ApiKeyDTO } from '../../../api/services/adminServiceHelpers';
 import { EditApiKeyFormDTO } from '../../services/apiKeyService';
 import { sortApiRoles } from '../../utils/apiRoles';
 import { Dialog } from '../Core/Dialog';
-import { Form } from '../Core/Form';
 import { MultiCheckboxInput } from '../Input/MultiCheckboxInput';
 import { TextInput } from '../Input/TextInput';
 import { getUnapprovedRoles } from './KeyEditDialogHelper';
@@ -18,13 +17,13 @@ export type OnApiKeyEdit = (
   setApiKey: React.Dispatch<React.SetStateAction<ApiKeyDTO>>
 ) => void;
 
-type KeyEditDialogProps = {
+type KeyEditDialogProps = Readonly<{
   onEdit: OnApiKeyEdit;
   triggerButton: JSX.Element;
   availableRoles: ApiRoleDTO[];
   apiKey: ApiKeyDTO;
   setApiKey: React.Dispatch<React.SetStateAction<ApiKeyDTO>>;
-};
+}>;
 
 function KeyEditDialog({
   availableRoles,
@@ -35,15 +34,18 @@ function KeyEditDialog({
 }: KeyEditDialogProps) {
   const [open, setOpen] = useState(false);
 
-  const onSubmit: SubmitHandler<EditApiKeyFormDTO> = async (formData) => {
-    await onEdit(formData, setApiKey);
-    setOpen(false);
-  };
+  const formMethods = useForm<EditApiKeyFormDTO>({
+    defaultValues: {
+      keyId: apiKey.key_id,
+      newName: apiKey.name,
+      newApiRoles: apiKey.roles.map((role) => role.roleName),
+    },
+  });
+  const { handleSubmit } = formMethods;
 
-  const defaultFormData: EditApiKeyFormDTO = {
-    keyId: apiKey.key_id,
-    newName: apiKey.name,
-    newApiRoles: apiKey.roles.map((role) => role.roleName),
+  const onSubmit: SubmitHandler<EditApiKeyFormDTO> = async (formData) => {
+    onEdit(formData, setApiKey);
+    setOpen(false);
   };
 
   const unapprovedRoles: ApiRoleDTO[] = getUnapprovedRoles(apiKey.roles, availableRoles);
@@ -57,36 +59,39 @@ function KeyEditDialog({
         triggerButton={triggerButton}
         title={`Edit ${apiKey.name}`}
       >
-        <Form<EditApiKeyFormDTO>
-          onSubmit={onSubmit}
-          defaultValues={defaultFormData}
-          submitButtonText='Save Key'
-        >
-          <TextInput inputName='newName' label='Name' required />
-          <MultiCheckboxInput
-            label='API Permissions'
-            inputName='newApiRoles'
-            options={sortApiRoles(availableRoles.concat(unapprovedRoles)).map((role) => ({
-              optionLabel: role.externalName,
-              value: role.roleName,
-            }))}
-            rules={{
-              required: 'Please select at least one API permission.',
-            }}
-          />
-          {unapprovedRoles.length > 0 && (
-            <div className='unapproved-roles-message'>
-              You do not have permission for:
-              <ul>
-                {unapprovedRoles.map((role) => (
-                  <li key={`Role-${role.id}`}>{role.externalName}</li>
-                ))}
-              </ul>
-              If you remove any of these permissions from the key, you will not be able to undo this
-              action.
+        <FormProvider {...formMethods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextInput inputName='newName' label='Name' required />
+            <MultiCheckboxInput
+              label='API Permissions'
+              inputName='newApiRoles'
+              options={sortApiRoles(availableRoles.concat(unapprovedRoles)).map((role) => ({
+                optionLabel: role.externalName,
+                value: role.roleName,
+              }))}
+              rules={{
+                required: 'Please select at least one API permission.',
+              }}
+            />
+            {unapprovedRoles.length > 0 && (
+              <div className='unapproved-roles-message'>
+                You do not have permission for:
+                <ul>
+                  {unapprovedRoles.map((role) => (
+                    <li key={`Role-${role.id}`}>{role.externalName}</li>
+                  ))}
+                </ul>
+                If you remove any of these permissions from the key, you will not be able to undo
+                this action.
+              </div>
+            )}
+            <div className='form-footer'>
+              <button type='submit' className='primary-button'>
+                Save Key
+              </button>
             </div>
-          )}
-        </Form>
+          </form>
+        </FormProvider>
       </Dialog>
     </div>
   );
