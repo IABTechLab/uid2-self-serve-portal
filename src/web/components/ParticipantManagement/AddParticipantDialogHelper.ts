6a333f7b8e76d6485f9b146445f632a8e57a616a
Ashley Smith <ashley.smith@thetradedesk.com>
2024-04-03 14:21:16 -0600

    added validate check if crm agreement number does not exist it will not affect editing other values

--- src/web/components/ParticipantManagement/AddParticipantDialogHelper.ts
+++ src/web/components/ParticipantManagement/AddParticipantDialogHelper.ts
@@ -1,4 +1,6 @@
 export const validatecrmAgreementNumber = (value: string): boolean | string => {
+  if (value === '') return true;
+
   const regex = /^\d{8}$/;
   return regex.test(value) ? true : 'Please enter an 8-digit number.';
 };
