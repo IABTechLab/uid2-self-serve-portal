ccd1ee5d7c6f0c20a08db3cddd11e156753562ab
Ashley Smith <ashley.smith@thetradedesk.com>
2024-05-07 17:45:08 -0600

    get paged domains test

--- src/web/components/ClientSideTokenGeneration/CstgDomainHelper.spec.tsx
+++ src/web/components/ClientSideTokenGeneration/CstgDomainHelper.spec.tsx
@@ -1,4 +1,4 @@
-import { extractTopLevelDomain, isValidDomain } from './CstgDomainHelper';
+import { extractTopLevelDomain, getPagedDomains, isValidDomain } from './CstgDomainHelper';
 
 const validDomainsList = ['test.com', 'https://test.com', 'http://test.com'];
 
@@ -17,6 +17,68 @@ describe('test domain helper functions', () => {
     );
     it.each(validDomainsList)('should return the domain is valid', (stringList) => {
       expect(isValidDomain(stringList)).toEqual(true);
+    });
+  });
+
+  describe('filter correct paged domains', () => {
+    const domainsList = [
+      'test.com',
+      'test2.com',
+      'test3.com',
+      'test4.com',
+      'test5.com',
+      'test6.com',
+      'test7.com',
+      'test8.com',
+      'test9.com',
+      'test10.com',
+      'test11.com',
+      'test12.com',
+      'test13.com',
+      'test14.com',
+      'test15.com',
+      'test16.com',
+      'test17.com',
+      'test18.com',
+      'test19.com',
+      'test20.com',
+      'test21.com',
+      'test22.com',
+      'test23.com',
+      'test24.com',
+      'test25.com',
+      'test26.com',
+      'test27.com',
+      'test28.com',
+      'test29.com',
+    ];
+    it('should return a filtered first page domain list', () => {
+      expect(getPagedDomains(domainsList, 1, 10)).toEqual([
+        'test.com',
+        'test2.com',
+        'test3.com',
+        'test4.com',
+        'test5.com',
+        'test6.com',
+        'test7.com',
+        'test8.com',
+        'test9.com',
+        'test10.com',
+      ]);
+    });
+    it('should return a filtered domain list at the end', () => {
+      expect(getPagedDomains(domainsList, 2, 25)).toEqual([
+        'test26.com',
+        'test27.com',
+        'test28.com',
+        'test29.com',
+      ]);
+    });
+    it('should return a filtered total domain list', () => {
+      expect(getPagedDomains(domainsList, 1, 250)).toEqual(domainsList);
+    });
+    it('should return an empty list', () => {
+      expect(getPagedDomains(domainsList, 0, 10)).toEqual([]);
     });
   });
 });
