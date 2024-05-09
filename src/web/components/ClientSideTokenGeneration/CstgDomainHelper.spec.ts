acf6d1f82a6a2f238b4a9f3aa4530af45201bae9
Ashley Smith <ashley.smith@thetradedesk.com>
2024-05-09 14:19:00 -0600

    used variables where possible

--- src/web/components/ClientSideTokenGeneration/CstgDomainHelper.spec.ts
+++ src/web/components/ClientSideTokenGeneration/CstgDomainHelper.spec.ts
@@ -55,28 +55,18 @@ describe('test domain helper functions', () => {
     it('should return a filtered first page domain list', () => {
       const pageNumber = 1;
       const rowsPerPage = 10;
-      expect(getPagedDomains(longDomainsList, pageNumber, rowsPerPage)).toEqual([
-        'test.com',
-        'test2.com',
-        'test3.com',
-        'test4.com',
-        'test5.com',
-        'test6.com',
-        'test7.com',
-        'test8.com',
-        'test9.com',
-        'test10.com',
-      ]);
+      expect(getPagedDomains(longDomainsList, pageNumber, rowsPerPage)).toEqual(
+        longDomainsList.filter((_, index) => index < pageNumber * rowsPerPage)
+      );
     });
     it('should return a filtered domain list at the end less than the number of rows per page', () => {
       const pageNumber = 2;
       const rowsPerPage = 25;
-      expect(getPagedDomains(longDomainsList, pageNumber, rowsPerPage)).toEqual([
-        'test26.com',
-        'test27.com',
-        'test28.com',
-        'test29.com',
-      ]);
+      expect(getPagedDomains(longDomainsList, pageNumber, rowsPerPage)).toEqual(
+        longDomainsList.filter(
+          (_, index) => index >= (pageNumber - 1) * rowsPerPage && index < pageNumber * rowsPerPage
+        )
+      );
     });
     it('should return a filtered total domain list since rows per page is great than total', () => {
       const pageNumber = 1;
