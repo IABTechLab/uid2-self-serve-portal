b0bfb4e59c80aeec7eee3cb45b0cd792a5c8c2fa
Ashley Smith <ashley.smith@thetradedesk.com>
2024-05-29 11:48:47 -0600

    changed error message

--- src/web/components/TeamMember/TeamMemberHelper.ts
+++ src/web/components/TeamMember/TeamMemberHelper.ts
@@ -5,7 +5,7 @@ export const validateUniqueTeamMemberEmail = (
   existingTeamMembers: UserDTO[] | undefined
 ) => {
   if (existingTeamMembers && existingTeamMembers?.filter((t) => t.email === value).length > 0) {
-    return 'Please enter a team member email that does not already exist.';
+    return 'Team member email already exists.';
   }
   return true;
 };
