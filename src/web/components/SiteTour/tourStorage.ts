b94df1af2e6fc873b08f657a14bdcd6010ce0a23
Scott Sundahl <scott.sundahl@thetradedesk.com>
2024-07-05 11:59:44 -0600

    check for new tour versions

--- src/web/components/SiteTour/tourStorage.ts
+++ src/web/components/SiteTour/tourStorage.ts
@@ -48,8 +48,46 @@ export function markTourAsSeen() {
   saveTourData(tourData);
 }
 
+const compareVersions = (a: string | undefined, b: string): string => {
+  const defaultVersion = '0.0.0';
+
+  if (!a) {
+    return defaultVersion;
+  }
+
+  const parsedA: string[] = a.split('.');
+  const parsedB: string[] = b.split('.');
+
+  if (Number(parsedA[0]) > Number(parsedB[0])) {
+    return a;
+  }
+  if (Number(parsedB[0]) > Number(parsedA[0])) {
+    return b;
+  }
+  if (Number(parsedA[1]) > Number(parsedB[1])) {
+    return a;
+  }
+  if (Number(parsedB[1]) > Number(parsedA[1])) {
+    return b;
+  }
+  if (Number(parsedA[2]) > Number(parsedB[2])) {
+    return a;
+  }
+  if (Number(parsedB[2]) > Number(parsedA[2])) {
+    return b;
+  }
+  return defaultVersion;
+};
+
 export function GetTourSteps(): VersionedTourStep[] {
-  return tourSteps.filter(
-    (step) => !getTourData().seenForVersions.includes(step.version ?? version)
-  );
+  // search seen steps for the highest version number
+  const storedVersions = getTourData().seenForVersions;
+  let highestStoredVersion = '0.0.0';
+  storedVersions.forEach((v) => {
+    highestStoredVersion = compareVersions(highestStoredVersion, v);
+  });
+  // return steps with versions higher than the highest seen version
+  return tourSteps.filter((step) => {
+    return compareVersions(step?.version, highestStoredVersion) === step?.version;
+  });
 }
