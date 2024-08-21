8bd1fa8ea82b6ec81e929ce8e1cd20b56c9b771f
Alex Yau <alex.yau@thetradedesk.com>
2024-08-21 16:52:06 +1000

    Fix build error

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { AuthClientTokens } from '@react-keycloak/core';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
 import { useCallback } from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { App } from './web/App';
 import { setAuthToken } from './web/axios';
