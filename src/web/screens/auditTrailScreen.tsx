ddab1367ea4cb350bcaa1859de9ffd4fb0845a0d
Scott Sundahl <scott.sundahl@thetradedesk.com>
2024-10-11 16:32:30 -0600

    participant audit trail

---
+++ src/web/screens/auditTrailScreen.tsx
@@ -0,0 +1,42 @@
+import { Suspense } from 'react';
+import { defer, useLoaderData } from 'react-router-typesafe';
+
+import AuditLogTable from '../components/AuditTrail/AuditTrailTable';
+import { Loading } from '../components/Core/Loading/Loading';
+import { ScreenContentContainer } from '../components/Core/ScreenContentContainer/ScreenContentContainer';
+import { GetAuditLogs } from '../services/auditTrailService';
+import { AwaitTypesafe } from '../utils/AwaitTypesafe';
+import { makeParticipantLoader } from '../utils/loaderHelpers';
+import { RouteErrorBoundary } from '../utils/RouteErrorBoundary';
+import { PortalRoute } from './routeUtils';
+
+const loader = makeParticipantLoader((participantId) => {
+  const auditTrail = GetAuditLogs(participantId);
+  return defer({ auditTrail });
+});
+
+function AuditTrail() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      <h1>Audit Trail</h1>
+      <ScreenContentContainer>
+        <Suspense fallback={<Loading message='Loading audit trail...' />}>
+          <AwaitTypesafe resolve={data.auditTrail}>
+            {(auditTrail) => <AuditLogTable auditTrail={auditTrail} />}
+          </AwaitTypesafe>
+        </Suspense>
+      </ScreenContentContainer>
+    </>
+  );
+}
+
+export const AuditTrailRoute: PortalRoute = {
+  description: 'Audit Trail',
+  element: <AuditTrail />,
+  errorElement: <RouteErrorBoundary />,
+  path: '/participant/:participantId/auditTrail',
+  loader,
+  isHidden: true,
+};
---
+++ src/web/services/auditTrailService.ts
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+import { AuditTrailDTO, AuditTrailEvents } from '../../api/entities/AuditTrail';
+import { mapClientTypeIdsToAdminEnums } from '../../api/services/adminServiceHelpers';
+import { backendError } from '../utils/apiError';
+import { getRoleNamesByIds } from '../utils/apiRoles';
+
+export const GetAuditLogs = async (participantId: number) => {
+  try {
+    const result = await axios.get<AuditTrailDTO[]>(`/participants/${participantId}/auditTrail`);
+    return result.data;
+  } catch (e: unknown) {
+    throw backendError(e, 'Could not get audit trail.');
+  }
+};
+
+export const getPrettyAuditDetails = (eventType: AuditTrailEvents, eventData: unknown) => {
+  const data = eventData as Record<string, unknown>;
+  const outputArray = [];
+  // eslint-disable-next-line guard-for-in
+  for (const key in data) {
+    let val = data[key];
+    // make data more human-readable
+    if (key.toLowerCase().indexOf('apiroles') > -1) {
+      const roleData = val as string[];
+      // some audit records use ids rather than names for the roles
+      if (roleData.length > 0 && !isNaN(Number(roleData[0]))) {
+        val = getRoleNamesByIds(val as number[]);
+      }
+    } else if (key.toLowerCase().indexOf('participanttypes') > -1) {
+      const typeData = val as string[];
+      // some audit records use ids rather than names for the types
+      if (typeData.length > 0 && !isNaN(Number(typeData[0]))) {
+        val = mapClientTypeIdsToAdminEnums(val as number[]);
+      }
+    }
+    // convert key from camelCase
+    outputArray.push(
+      `${key[0].toUpperCase() + key.substring(1).replace(/([a-z])([A-Z])/g, '$1 $2')}: ${val}`
+    );
+  }
+  const outputString = outputArray.join(' | ');
+  return outputString;
+};
