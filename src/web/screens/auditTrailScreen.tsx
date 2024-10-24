d4c57dbb497c97fbb331aeefeb1877204fc6d9fe
Ashley Smith <ashley.smith@thetradedesk.com>
2024-10-24 12:29:15 -0600

    rerouting in audit trail if user does not have role

--- src/web/screens/auditTrailScreen.tsx
+++ src/web/screens/auditTrailScreen.tsx
@@ -1,9 +1,13 @@
-import { Suspense } from 'react';
+import { Suspense, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { defer, useLoaderData } from 'react-router-typesafe';
 
 import AuditTrailTable from '../components/AuditTrail/AuditTrailTable';
 import { Loading } from '../components/Core/Loading/Loading';
+import { ErrorToast } from '../components/Core/Popups/Toast';
 import { ScreenContentContainer } from '../components/Core/ScreenContentContainer/ScreenContentContainer';
+import { CurrentUserContext } from '../contexts/CurrentUserProvider';
+import { ParticipantContext } from '../contexts/ParticipantProvider';
 import { GetAuditTrail } from '../services/auditTrailService';
 import { AwaitTypesafe } from '../utils/AwaitTypesafe';
 import { makeParticipantLoader } from '../utils/loaderHelpers';
@@ -17,6 +21,16 @@ const loader = makeParticipantLoader((participantId) => {
 
 function AuditTrail() {
   const data = useLoaderData<typeof loader>();
+  const { participant } = useContext(ParticipantContext);
+  const { LoggedInUser } = useContext(CurrentUserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!LoggedInUser?.user?.isUid2Support && participant) {
+      navigate(`/participant/${participant.id}/home`);
+      ErrorToast(`You do not have access to this page. Rerouting back to Home.`);
+    }
+  });
 
   return (
     <>
