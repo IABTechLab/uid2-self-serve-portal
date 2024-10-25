03bb522a31de6a98e3acaad7daba5c84e9bae633
Ashley Smith <ashley.smith@thetradedesk.com>
2024-10-24 19:07:07 -0600

    removed unnecessary permissions check

--- src/api/routers/participants/participantsAuditTrail.ts
+++ src/api/routers/participants/participantsAuditTrail.ts
@@ -1,19 +1,9 @@
 import { Response } from 'express';
 
-import { UserRoleId } from '../../entities/UserRole';
 import { GetParticipantAuditTrail } from '../../services/auditTrailService';
 import { UserParticipantRequest } from '../../services/participantsService';
 
 export async function handleAuditTrail(req: UserParticipantRequest, res: Response) {
-  const { participant, user } = req;
-  const userParticipant = user?.participants?.find((item) => item.id === participant?.id);
-  if (
-    userParticipant?.currentUserRoleIds?.filter((item) => {
-      return item === UserRoleId.Admin || item === UserRoleId.UID2Support;
-    }).length === 0
-  ) {
-    return res.status(500).json({ message: 'Permission Denied' });
-  }
   const auditTrail = await GetParticipantAuditTrail(req.participant!);
   return res.status(200).json(auditTrail ?? []);
 }
