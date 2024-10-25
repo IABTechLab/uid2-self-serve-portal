d1733c775bef6e88d90e458c5bb91115ce3358d0
Ashley Smith <ashley.smith@thetradedesk.com>
2024-10-25 13:51:55 -0600

    adding portal header test

--- src/web/components/PortalHeader/PortalHeader.spec.tsx
+++ src/web/components/PortalHeader/PortalHeader.spec.tsx
@@ -2,8 +2,10 @@ import { composeStories } from '@storybook/react';
 import { render, screen } from '@testing-library/react';
 
 import * as stories from './PortalHeader.stories';
+import { createMockParticipant, createMockUser } from '../../../testHelpers/dataMocks';
+import { Participant } from '../../../api/entities/Participant';
 
-const { InvalidEmailAddress, NoEmailAddress } = composeStories(stories);
+const { InvalidEmailAddress, NoEmailAddress, ValidEmailAddress } = composeStories(stories);
 
 describe('Portal Header tests', () => {
   test('when an invalid email address is provided, a home link is still displayed', async () => {
@@ -14,6 +16,15 @@ describe('Portal Header tests', () => {
 
   test('when no email is provided, the dropdown text shows that there is no logged in user', async () => {
     render(<NoEmailAddress />);
+    const button = screen.getByRole('button');
+    expect(button).toHaveTextContent('Not logged in');
+  });
+
+  test('when user has uid2support role', () => {
+    render(<ValidEmailAddress />);
+    const mockParticipant = createMockParticipant();
+    const mockUser = createMockUser([mockParticipant] as Participant[]);
+    mockUser.isUid2Support = true;
     const button = screen.getByRole('button');
     expect(button).toHaveTextContent('Not logged in');
   });
