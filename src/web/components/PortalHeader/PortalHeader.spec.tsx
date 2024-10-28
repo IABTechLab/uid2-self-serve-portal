8cb690b96c5c90279b7541859639757c95b0d80f
Ashley Smith <ashley.smith@thetradedesk.com>
2024-10-28 14:24:43 -0600

    added tests for roles with portal header

--- src/web/components/PortalHeader/PortalHeader.spec.tsx
+++ src/web/components/PortalHeader/PortalHeader.spec.tsx
@@ -14,6 +14,7 @@ import { UserContextWithSetter } from '../../contexts/CurrentUserProvider';
 import { ParticipantWithSetter } from '../../contexts/ParticipantProvider';
 import { PortalHeader } from './PortalHeader';
 import * as stories from './PortalHeader.stories';
+import { UserRoleId } from '../../../api/entities/UserRole';
 
 const { InvalidEmailAddress, NoEmailAddress } = composeStories(stories);
 
@@ -58,7 +59,7 @@ describe('Portal Header tests', () => {
       userContextValue = createUserContextValue(mockUser);
     });
 
-    test('when user has uid2support role', async () => {
+    it('should show Audit Trail when user has uid2support role', async () => {
       mockUser.isUid2Support = true;
       renderPortalHeaderWithContext(userContextValue, participantContextValue);
       const dropdownMenuButton = screen.getByRole('button');
@@ -69,8 +70,37 @@ describe('Portal Header tests', () => {
       expect(auditTrailMenuItem).toBeInTheDocument();
     });
 
-    test('when user does not have uid2support role', async () => {
+    it('should not show Audit Trail when user does not have uid2support role', async () => {
       mockUser.isUid2Support = false;
+      renderPortalHeaderWithContext(userContextValue, participantContextValue);
+      const dropdownMenuButton = screen.getByRole('button');
+      await user.click(dropdownMenuButton);
+      expect(screen.queryByRole('menuitem', { name: 'Audit Trail' })).not.toBeInTheDocument();
+    });
+
+    const teamMemberManageRoles = [
+      { id: UserRoleId.UID2Support, roleName: 'UID2Support' },
+      { id: UserRoleId.Admin, roleName: 'Admin' },
+    ];
+    it.each(teamMemberManageRoles)(
+      'should show Audit Trail in dropdown menu when user has UID2Support or Admin roles for the participant',
+      async (manageRole) => {
+        mockUser.isUid2Support = false;
+        mockUser.currentParticipantUserRoles = [manageRole];
+        userContextValue = createUserContextValue(mockUser);
+        renderPortalHeaderWithContext(userContextValue, participantContextValue);
+        const dropdownMenuButton = screen.getByRole('button');
+        await user.click(dropdownMenuButton);
+        expect(screen.queryByRole('menuitem', { name: 'Audit Trail' })).not.toBeInTheDocument();
+      }
+    );
+
+    it('should not show Audit Trail in dropdown menu when user has Operations role for the participant', async () => {
+      mockUser.isUid2Support = false;
+      mockUser.currentParticipantUserRoles = [
+        { id: UserRoleId.Operations, roleName: 'Operations' },
+      ];
+      userContextValue = createUserContextValue(mockUser);
       renderPortalHeaderWithContext(userContextValue, participantContextValue);
       const dropdownMenuButton = screen.getByRole('button');
       await user.click(dropdownMenuButton);
