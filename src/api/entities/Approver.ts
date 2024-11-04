d401a0ecd844602af84604b0d550fba7b999c130
Ashley Smith <ashley.smith@thetradedesk.com>
2024-11-04 09:45:58 -0700

    start of removing approvers references

--- src/api/entities/Approver.ts
+++ src/api/entities/Approver.ts
@@ -1,23 +1,23 @@
-import { Model } from 'objection';
+// import { Model } from 'objection';
 
-import { BaseModel } from './BaseModel';
+// import { BaseModel } from './BaseModel';
 
-export class Approver extends BaseModel {
-  static get tableName() {
-    return 'approvers';
-  }
-  static relationMappings = {
-    type: {
-      relation: Model.BelongsToOneRelation,
-      modelClass: 'ParticipantType',
-      join: {
-        from: 'approvers.participantTypeId',
-        to: 'participantsToTypes.id',
-      },
-    },
-  };
-  declare id: number;
-  declare email: string;
-  declare participantTypeId: number;
-  declare displayName: string;
-}
+// export class Approver extends BaseModel {
+//   static get tableName() {
+//     return 'approvers';
+//   }
+//   static relationMappings = {
+//     type: {
+//       relation: Model.BelongsToOneRelation,
+//       modelClass: 'ParticipantType',
+//       join: {
+//         from: 'approvers.participantTypeId',
+//         to: 'participantsToTypes.id',
+//       },
+//     },
+//   };
+//   declare id: number;
+//   declare email: string;
+//   declare participantTypeId: number;
+//   declare displayName: string;
+// }
