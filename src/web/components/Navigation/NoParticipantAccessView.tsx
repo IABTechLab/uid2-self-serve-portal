25a432ddfdf733ae91e41cfc65979d163169aa5c
Ashley Smith <ashley.smith@thetradedesk.com>
2024-11-04 11:53:05 -0700

    fix for single participant

--- src/web/components/Navigation/NoParticipantAccessView.tsx
+++ src/web/components/Navigation/NoParticipantAccessView.tsx
@@ -1,25 +1,51 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { UserWithParticipantRoles } from '../../../api/services/usersService';
+import { getPathWithParticipant } from '../../utils/urlHelpers';
 import { ParticipantSwitcher } from './ParticipantSwitcher';
 
 import './NoParticipantAccessView.scss';
-import { UserWithParticipantRoles } from '../../../api/services/usersService';
 
 type NoParticipantAccessViewProps = Readonly<{
   user: UserWithParticipantRoles | null;
 }>;
 
 export function NoParticipantAccessView({ user }: NoParticipantAccessViewProps) {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onBackToParticipant = () => {
+    let participantId;
+    if (user?.participants) {
+      participantId = user.participants[0].id;
+    } else {
+      participantId = localStorage.getItem('lastSelectedParticipantId');
+    }
+    if (participantId) {
+      const newPath = getPathWithParticipant(location.pathname, participantId);
+      navigate(newPath);
+    }
+  };
+
   return (
     <div className='no-participant-access-container'>
       <p className='no-access-text instructions'>You do not have access to this participant.</p>
-      <p className='use-switcher-text instructions'>
-        Use the dropdown below to navigate to a participant you have access to.
-      </p>
+
       {(user?.participants?.length ?? 0) > 1 ? (
-        <div className='switcher'>
-          <ParticipantSwitcher noInitialValue />
-        </div>
+        <>
+          <p className='use-switcher-text instructions'>
+            Use the dropdown below to navigate to a participant you have access to.
+          </p>
+          <div className='switcher'>
+            <ParticipantSwitcher noInitialValue />
+          </div>
+        </>
       ) : (
-        <div></div>
+        <div>
+          <button className='small-button' type='button' onClick={onBackToParticipant}>
+            Back to Your Participant
+          </button>
+        </div>
       )}
     </div>
   );
