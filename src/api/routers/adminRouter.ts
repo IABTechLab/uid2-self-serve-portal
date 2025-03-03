1d1e9b52a7564282a735030e16109d7f5b1d2384
Scott Sundahl <scott.sundahl@thetradedesk.com>
2025-03-03 16:13:41 -0700

    middleware test

--- src/api/routers/adminRouter.ts
+++ src/api/routers/adminRouter.ts
@@ -1,24 +1,18 @@
-import express from 'express';
+import express, { Response } from 'express';
 
 import { isSuperUserCheck } from '../middleware/userRoleMiddleware';
+import { ParticipantRequest } from '../services/participantsService';
 import { getAdminUserList } from '../services/usersService';
-// import { getSiteList, getVisibleSiteList } from '../services/adminServiceClient';
-// import { AdminSiteDTO, mapAdminSitesToSiteDTOs } from '../services/adminServiceHelpers';
-// import { getAttachedSiteIDs, getParticipantsBySiteIds } from '../services/participantsService';
+
+const handleGetAllUsersAdmin = async (req: ParticipantRequest, res: Response) => {
+  const userList = await getAdminUserList();
+  return res.status(200).json(userList);
+};
 
 export function createAdminRouter() {
   const adminRouter = express.Router();
 
-  adminRouter.get('/users', isSuperUserCheck, async (_req, res) => {
-    // const allSitesPromise = getSiteList();
-    // const attachedSitesPromise = getAttachedSiteIDs();
-    // const [allSites, attachedSites] = await Promise.all([allSitesPromise, attachedSitesPromise]);
-    // const siteDTOs = await mapAdminSitesToSiteDTOs(
-    //   allSites.filter((s) => !attachedSites.includes(s.id))
-    // );
-    const userList = await getAdminUserList();
-    return res.status(200).json(userList);
-  });
+  adminRouter.get('/users', isSuperUserCheck, handleGetAllUsersAdmin);
 
   return adminRouter;
 }
