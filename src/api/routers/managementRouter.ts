5a6a9a8b2b69d606de875aa081b4b07e98907795
Ashley Smith <ashley.smith@thetradedesk.com>
2025-03-10 15:50:12 -0600

    dont need both request types

--- src/api/routers/managementRouter.ts
+++ src/api/routers/managementRouter.ts
@@ -4,20 +4,20 @@ import { z } from 'zod';
 import { isSuperUserCheck } from '../middleware/userRoleMiddleware';
 import { GetUserAuditTrail } from '../services/auditTrailService';
 import { getAllUsersList, getUserById, updateUserLock } from '../services/managementService';
-import { getUserParticipants , ParticipantRequest, UserParticipantRequest } from '../services/participantsService';
+import { getUserParticipants, ParticipantRequest } from '../services/participantsService';
 
 const handleGetAllUsers = async (req: ParticipantRequest, res: Response) => {
   const userList = await getAllUsersList();
   return res.status(200).json(userList);
 };
 
-const handleGetUserAuditTrail = async (req: UserParticipantRequest, res: Response) => {
+const handleGetUserAuditTrail = async (req: ParticipantRequest, res: Response) => {
   const { userId } = z.object({ userId: z.coerce.number() }).parse(req.params);
   const auditTrail = await GetUserAuditTrail(userId);
   return res.status(200).json(auditTrail ?? []);
 };
 
-const handleGetUserParticipants = async (req: UserParticipantRequest, res: Response) => {
+const handleGetUserParticipants = async (req: ParticipantRequest, res: Response) => {
   const { userId } = z.object({ userId: z.coerce.number() }).parse(req.params);
   const participants = await getUserParticipants(userId);
   return res.status(200).json(participants ?? []);
