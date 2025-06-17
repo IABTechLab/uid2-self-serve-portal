09e20c9765f90f08ef68fe30fa29d39e85f759ad
Ashley Smith <ashley.smith@thetradedesk.com>
2025-06-17 15:34:41 -0600

    storybook fixes may need to wait for esm update

--- src/web/components/Core/Card/Card.spec.tsx
+++ src/web/components/Core/Card/Card.spec.tsx
@@ -1,4 +1,4 @@
-import { composeStories } from '@storybook/react-webpack5';
+import { composeStories } from '@storybook/react';
 import { render, screen } from '@testing-library/react';
 
 import * as stories from './Card.stories';
--- src/web/components/Input/SelectInput.spec.tsx
+++ src/web/components/Input/SelectInput.spec.tsx
@@ -1,4 +1,4 @@
-import { composeStories } from '@storybook/react-webpack5';
+import { composeStories } from '@storybook/react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
