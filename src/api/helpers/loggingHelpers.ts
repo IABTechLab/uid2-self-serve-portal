a411a25007a2677706bd2a5ade1765ccd71292ee
Ashley Smith <ashley.smith@thetradedesk.com>
2025-06-23 15:08:44 -0600

    another fix to pass all api tests

--- src/api/helpers/loggingHelpers.ts
+++ src/api/helpers/loggingHelpers.ts
@@ -82,9 +82,10 @@ export interface TraceId {
 }
 
 export const getTraceId = (request: Request): TraceId => {
-  const traceId = request?.headers?.traceId?.toString() || '';
+  const traceId = request?.headers?.traceId?.toString() || ''
+	const uidTraceId = (request?.headers?.['X-Amzn-Trace-Id'] as string | undefined) ?? traceId ?? '';
   return {
     traceId,
-    uidTraceId: request?.headers['X-Amzn-Trace-Id']?.toString() || traceId || '',
+    uidTraceId
   };
 };
