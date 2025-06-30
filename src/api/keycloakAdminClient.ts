30739b42f71a87c001e9afdcc56101b88d5a8c7c
Ashley Smith <ashley.smith@thetradedesk.com>
2025-06-30 12:44:40 -0600

    keycloak admin client cleanup

--- src/api/keycloakAdminClient.ts
+++ src/api/keycloakAdminClient.ts
@@ -1,22 +1,36 @@
+import KeycloakAdminClient from '@keycloak/keycloak-admin-client';
 import { SSP_KK_AUDIENCE, SSP_KK_AUTH_SERVER_URL, SSP_KK_REALM, SSP_KK_SECRET } from './envars';
+
+let KcAdminClientClass: typeof KeycloakAdminClient | null = null;
+let kcClientInstance: KeycloakAdminClient | null = null;
 
 // dynamic import of KcAdminClient required by keycloak-admin-client version 26.1.3
 const loadKcAdminClient = async () => {
-  const KcAdminClientClass = (await import('@keycloak/keycloak-admin-client')).default;
-
-  return new KcAdminClientClass({
-    baseUrl: SSP_KK_AUTH_SERVER_URL,
-    realmName: SSP_KK_REALM,
-  });
+	KcAdminClientClass ??= (await import('@keycloak/keycloak-admin-client')).default;
 };
 
 export const getKcAdminClient = async () => {
-  const kcClient = await loadKcAdminClient();
-  await kcClient.auth({
+	if (kcClientInstance) {
+		return kcClientInstance;
+	}
+
+	if (!KcAdminClientClass) {
+		await loadKcAdminClient();
+		if (!KcAdminClientClass) {
+    	throw new Error('Failed to load Keycloak Admin Client class');
+		}
+  }
+
+	kcClientInstance = new KcAdminClientClass({
+    baseUrl: SSP_KK_AUTH_SERVER_URL,
+    realmName: SSP_KK_REALM,
+  });
+
+  await kcClientInstance.auth({
     grantType: 'client_credentials',
     clientId: SSP_KK_AUDIENCE,
     clientSecret: SSP_KK_SECRET,
   });
-	
-  return kcClient;
+
+  return kcClientInstance;
 };
