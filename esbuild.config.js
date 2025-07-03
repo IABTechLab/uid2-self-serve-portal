2433a9f52a66d6c4fba261758f0fb8098569f378
Ashley Smith <ashley.smith@thetradedesk.com>
2025-07-03 12:11:42 -0600

    switched to esbuild instead of tsc

---
+++ esbuild.config.js
@@ -0,0 +1,17 @@
+import { build } from 'esbuild';
+import { globSync } from 'glob';
+
+const entryPoints = globSync('src/api/**/*.ts');
+
+build({
+  entryPoints,
+  outdir: 'api-dist',
+  platform: 'node',
+  format: 'esm',
+  target: 'node20',
+  outbase: 'src',
+  sourcemap: true,
+  bundle: false,
+  loader: { '.ts': 'ts' },
+  logLevel: 'info'
+}).catch(() => process.exit(1));
