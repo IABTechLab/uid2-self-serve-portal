97f2aa7be81c070b0cedade46194c9fd694a06fc
Eiman Eltigani <v-eiman.eltigani@thetradedesk.com>
2025-07-09 15:25:30 -0600

    declare new primaryContact field in participant entity

--- src/api/entities/Participant.ts
+++ src/api/entities/Participant.ts
@@ -13,69 +13,69 @@ export class Participant extends BaseModel {
     return 'participants';
   }
   static get relationMappings(): RelationMappings {
-		return {
-			types: {
-				relation: Model.ManyToManyRelation,
-				modelClass: () => ParticipantType,
-				join: {
-					from: 'participants.id',
-					through: {
-						from: 'participantsToTypes.participantId',
-						to: 'participantsToTypes.participantTypeId',
-					},
-					to: 'participantTypes.id',
-				},
-			},
-			apiRoles: {
-				relation: Model.ManyToManyRelation,
-				modelClass: () => ApiRole,
-				join: {
-					from: 'participants.id',
-					through: {
-						from: 'participantsToApiRoles.participantId',
-						to: 'participantsToApiRoles.apiRoleId',
-					},
-					to: 'apiRoles.id',
-				},
-			},
-			users: {
-				relation: Model.ManyToManyRelation,
-				modelClass: () => User,
-				join: {
-					from: 'participants.id',
-					through: {
-						from: 'usersToParticipantRoles.participantId',
-						to: 'usersToParticipantRoles.userId',
-					},
-					to: 'users.id',
-				},
-			},
-			businessContacts: {
-				relation: Model.HasManyRelation,
-				modelClass: () => BusinessContact,
-				join: {
-					from: 'participants.id',
-					to: 'businessContacts.participantId',
-				},
-			},
-			approver: {
-				relation: Model.BelongsToOneRelation,
-				modelClass: () => User,
-				join: {
-					from: 'participants.approverId',
-					to: 'users.id',
-				},
-			},
-			participantToUserRoles: {
-				relation: Model.HasManyRelation,
-				modelClass: () => UserToParticipantRole,
-				join: {
-					from: 'participants.id',
-					to: 'usersToParticipantRoles.participantId',
-				},
-			},
-		};
-  };
+    return {
+      types: {
+        relation: Model.ManyToManyRelation,
+        modelClass: () => ParticipantType,
+        join: {
+          from: 'participants.id',
+          through: {
+            from: 'participantsToTypes.participantId',
+            to: 'participantsToTypes.participantTypeId',
+          },
+          to: 'participantTypes.id',
+        },
+      },
+      apiRoles: {
+        relation: Model.ManyToManyRelation,
+        modelClass: () => ApiRole,
+        join: {
+          from: 'participants.id',
+          through: {
+            from: 'participantsToApiRoles.participantId',
+            to: 'participantsToApiRoles.apiRoleId',
+          },
+          to: 'apiRoles.id',
+        },
+      },
+      users: {
+        relation: Model.ManyToManyRelation,
+        modelClass: () => User,
+        join: {
+          from: 'participants.id',
+          through: {
+            from: 'usersToParticipantRoles.participantId',
+            to: 'usersToParticipantRoles.userId',
+          },
+          to: 'users.id',
+        },
+      },
+      businessContacts: {
+        relation: Model.HasManyRelation,
+        modelClass: () => BusinessContact,
+        join: {
+          from: 'participants.id',
+          to: 'businessContacts.participantId',
+        },
+      },
+      approver: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: () => User,
+        join: {
+          from: 'participants.approverId',
+          to: 'users.id',
+        },
+      },
+      participantToUserRoles: {
+        relation: Model.HasManyRelation,
+        modelClass: () => UserToParticipantRole,
+        join: {
+          from: 'participants.id',
+          to: 'usersToParticipantRoles.participantId',
+        },
+      },
+    };
+  }
   declare id: number;
   declare name: string;
   declare allowSharing: boolean;
@@ -90,6 +90,7 @@ export class Participant extends BaseModel {
   declare crmAgreementNumber: string | null;
   declare currentUserRoleIds?: number[];
   declare participantToUserRoles?: UserToParticipantRole[];
+  declare primaryContact?: User;
 }
 
 // TODO: Can ModelObjectOpt do relationships automatically?
