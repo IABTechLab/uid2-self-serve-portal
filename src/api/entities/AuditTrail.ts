f3183e1c0a32b8f0e5aef9534499ceb1c419d85e
Eiman Eltigani <v-eiman.eltigani@thetradedesk.com>
2025-07-17 16:26:31 -0600

    revert spacing

--- src/api/entities/AuditTrail.ts
+++ src/api/entities/AuditTrail.ts
@@ -4,55 +4,55 @@ import { BaseModel } from './BaseModel.ts';
 import { ModelObjectOpt } from './ModelObjectOpt.ts';
 
 export enum AuditAction {
-  Add = 'Add',
-  Delete = 'Delete',
-  Update = 'Update',
+	Add = 'Add',
+	Delete = 'Delete',
+	Update = 'Update',
 }
 
 export enum AuditTrailEvents {
-  UpdateSharingPermissions = 'UpdateSharingPermissions',
-  UpdateSharingTypes = 'UpdateSharingTypes',
-  ApproveAccount = 'ApproveAccount',
-  ManageKeyPair = 'ManageKeyPair',
-  ManageApiKey = 'ManageApiKey',
-  ManageParticipant = 'ManageParticipant',
-  UpdateDomainNames = 'UpdateDomainNames',
-  UpdateAppNames = 'UpdateAppNames',
-  ManageTeamMembers = 'ManageTeamMembers',
-  ChangeUserLock = 'ChangeUserLock',
-  UpdatePrimaryContact = 'UpdatePrimaryContact',
+	UpdateSharingPermissions = 'UpdateSharingPermissions',
+	UpdateSharingTypes = 'UpdateSharingTypes',
+	ApproveAccount = 'ApproveAccount',
+	ManageKeyPair = 'ManageKeyPair',
+	ManageApiKey = 'ManageApiKey',
+	ManageParticipant = 'ManageParticipant',
+	UpdateDomainNames = 'UpdateDomainNames',
+	UpdateAppNames = 'UpdateAppNames',
+	ManageTeamMembers = 'ManageTeamMembers',
+	ChangeUserLock = 'ChangeUserLock',
+	UpdatePrimaryContact = 'UpdatePrimaryContact',
 }
 
 export class AuditTrail extends BaseModel {
-  static get tableName() {
-    return 'auditTrails';
-  }
-  static get relationMappings(): RelationMappings {
-                return {
-                  user: {
-                    relation: Model.BelongsToOneRelation,
-                    modelClass: 'User',
-                    join: {
-                      from: 'auditTrails.userId',
-                      to: 'users.id',
-                    },
-                  },
-                };
-  }
+	static get tableName() {
+		return 'auditTrails';
+	}
+	static get relationMappings(): RelationMappings {
+		return {
+			user: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: 'User',
+				join: {
+					from: 'auditTrails.userId',
+					to: 'users.id',
+				},
+			},
+		};
+	}
 
-  static get jsonAttributes() {
-    return ['eventData'];
-  }
+	static get jsonAttributes() {
+		return ['eventData'];
+	}
 
-  declare id: number;
-  declare userId: number;
-  declare userEmail: string;
-  declare succeeded: boolean;
-  declare event: AuditTrailEvents;
-  declare eventData: unknown;
-  declare participantId?: number | null;
-  // eslint-disable-next-line camelcase
-  declare updated_at: Date;
+	declare id: number;
+	declare userId: number;
+	declare userEmail: string;
+	declare succeeded: boolean;
+	declare event: AuditTrailEvents;
+	declare eventData: unknown;
+	declare participantId?: number | null;
+	// eslint-disable-next-line camelcase
+	declare updated_at: Date;
 }
 
 export type AuditTrailDTO = ModelObjectOpt<AuditTrail>;
