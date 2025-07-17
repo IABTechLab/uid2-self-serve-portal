f9e6e15bd31f3fb2f1e3cfd92ae8bde0856c16c6
Eiman Eltigani <v-eiman.eltigani@thetradedesk.com>
2025-07-17 16:28:55 -0600

    restore previous style of audittrail doc

--- src/api/entities/AuditTrail.ts
+++ src/api/entities/AuditTrail.ts
@@ -4,30 +4,30 @@ import { BaseModel } from './BaseModel.ts';
 import { ModelObjectOpt } from './ModelObjectOpt.ts';
 
 export enum AuditAction {
-	Add = 'Add',
-	Delete = 'Delete',
-	Update = 'Update',
+  Add = 'Add',
+  Delete = 'Delete',
+  Update = 'Update',
 }
 
 export enum AuditTrailEvents {
-	UpdateSharingPermissions = 'UpdateSharingPermissions',
-	UpdateSharingTypes = 'UpdateSharingTypes',
-	ApproveAccount = 'ApproveAccount',
-	ManageKeyPair = 'ManageKeyPair',
-	ManageApiKey = 'ManageApiKey',
-	ManageParticipant = 'ManageParticipant',
-	UpdateDomainNames = 'UpdateDomainNames',
-	UpdateAppNames = 'UpdateAppNames',
-	ManageTeamMembers = 'ManageTeamMembers',
-	ChangeUserLock = 'ChangeUserLock',
-	UpdatePrimaryContact = 'UpdatePrimaryContact',
+  UpdateSharingPermissions = 'UpdateSharingPermissions',
+  UpdateSharingTypes = 'UpdateSharingTypes',
+  ApproveAccount = 'ApproveAccount',
+  ManageKeyPair = 'ManageKeyPair',
+  ManageApiKey = 'ManageApiKey',
+  ManageParticipant = 'ManageParticipant',
+  UpdateDomainNames = 'UpdateDomainNames',
+  UpdateAppNames = 'UpdateAppNames',
+  ManageTeamMembers = 'ManageTeamMembers',
+  ChangeUserLock = 'ChangeUserLock',
+  UpdatePrimaryContact = 'UpdatePrimaryContact',
 }
 
 export class AuditTrail extends BaseModel {
-	static get tableName() {
-		return 'auditTrails';
-	}
-	static get relationMappings(): RelationMappings {
+  static get tableName() {
+    return 'auditTrails';
+  }
+  static get relationMappings(): RelationMappings {
 		return {
 			user: {
 				relation: Model.BelongsToOneRelation,
@@ -38,21 +38,21 @@ export class AuditTrail extends BaseModel {
 				},
 			},
 		};
-	}
+  };
 
-	static get jsonAttributes() {
-		return ['eventData'];
-	}
+  static get jsonAttributes() {
+    return ['eventData'];
+  }
 
-	declare id: number;
-	declare userId: number;
-	declare userEmail: string;
-	declare succeeded: boolean;
-	declare event: AuditTrailEvents;
-	declare eventData: unknown;
-	declare participantId?: number | null;
-	// eslint-disable-next-line camelcase
-	declare updated_at: Date;
+  declare id: number;
+  declare userId: number;
+  declare userEmail: string;
+  declare succeeded: boolean;
+  declare event: AuditTrailEvents;
+  declare eventData: unknown;
+  declare participantId?: number | null;
+  // eslint-disable-next-line camelcase
+  declare updated_at: Date;
 }
 
 export type AuditTrailDTO = ModelObjectOpt<AuditTrail>;
