d49295604731b6102b73a1b18dc792b895641a28
Ian Nara <ian.nara@thetradedesk.com>
2025-08-01 14:53:07 -0600

    faro

--- src/web/services/environmentVariables.ts
+++ src/web/services/environmentVariables.ts
@@ -25,7 +25,7 @@ const configMaps: { [index: string]: ClientConfig } = {
     environment: 'integ',
   },
   'uid2-integ-core-use2-ssp-alb-652178420.us-east-2.elb.amazonaws.com': {
-    faroUrl: hostedFaroUrl,
+    faroUrl: '',
     environment: 'integ',
   },
   'portal.unifiedid.com': {
