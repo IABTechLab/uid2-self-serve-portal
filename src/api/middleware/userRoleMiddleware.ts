da4a145cf9fd33ca3162c60f5dc5bee1540b0d07
Eiman Eltigani <v-eiman.eltigani@thetradedesk.com>
2026-02-24 11:19:07 -0700

    resolve conflict in userrolemiddleware

--- src/api/middleware/userRoleMiddleware.ts
+++ src/api/middleware/userRoleMiddleware.ts
@@ -26,7 +26,11 @@ export const isSuperUserCheck: Handler = async (req: ParticipantRequest, res, ne
 // assign uid2 support if user has prod-uid2.0-support in Microsoft Entra ID
 export const isUid2Support = async (req: Request) => {
   const authGroups = (req.auth?.payload?.groups as string[] | undefined) ?? [];
-  if (isSuperUser(req) || authGroups.includes('developer') || authGroups.includes('prod-uid2.0-support')) {
+  if (
+    isSuperUser(req) ||
+    authGroups.includes('developer') ||
+    authGroups.includes('prod-uid2.0-support')
+  ) {
     return true;
   }
 
@@ -53,8 +57,7 @@ export const isAdminOrUid2SupportCheck: Handler = async (req: ParticipantRequest
   const user = await findUserByEmail(userEmail);
   const userParticipant = user?.participants?.find((item) => item.id === participant?.id);
   const userIsAdminOrUid2Support =
-    userParticipant?.currentUserRoleIds?.includes(UserRoleId.Admin) ||
-    (await isUid2Support(req));
+    userParticipant?.currentUserRoleIds?.includes(UserRoleId.Admin) || (await isUid2Support(req));
   if (!userIsAdminOrUid2Support) {
     return res.status(403).json({
       message: 'Unauthorized. You do not have the necessary permissions.',
